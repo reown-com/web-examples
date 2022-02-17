73bde541c861db8a9683fb7f171ac09e79c76ecf
Ilja <idaderko@gmail.com>
2022-02-17 15:33:27 +0200

    session and pairing management

--- wallets/react-wallet-v2/src/data/EIP155Data.ts
+++ wallets/react-wallet-v2/src/data/EIP155Data.ts
@@ -62,6 +62,6 @@ export const EIP155_SIGNING_METHODS = {
   ETH_SIGN_TYPED_DATA: 'eth_signTypedData',
   ETH_SIGN_TYPED_DATA_V3: 'eth_signTypedData_v3',
   ETH_SIGN_TYPED_DATA_V4: 'eth_signTypedData_v4',
-  ETH_SIGN_RAW_TRANSACTION: 'eth_sendRawTransaction',
+  ETH_SEND_RAW_TRANSACTION: 'eth_sendRawTransaction',
   ETH_SEND_TRANSACTION: 'eth_sendTransaction'
 }
--- wallets/react-wallet-v2/src/pages/pairings.tsx
+++ wallets/react-wallet-v2/src/pages/pairings.tsx
@@ -1,14 +1,43 @@
 import PageHeader from '@/components/PageHeader'
+import PairingCard from '@/components/PairingCard'
 import { walletConnectClient } from '@/utils/WalletConnectUtil'
-import { Fragment } from 'react'
+import { Text } from '@nextui-org/react'
+import { ERROR } from '@walletconnect/utils'
+import { Fragment, useState } from 'react'
 
 export default function PairingsPage() {
-  console.log(walletConnectClient.session.values)
-  console.log(walletConnectClient.session.history.pending)
+  const [pairings, setPairings] = useState(walletConnectClient.pairing.values)
+
+  async function onDelete(topic: string) {
+    await walletConnectClient.pairing.delete({
+      topic,
+      reason: ERROR.DELETED.format()
+    })
+    const newPairings = pairings.filter(pairing => pairing.topic !== topic)
+    setPairings(newPairings)
+  }
 
   return (
     <Fragment>
       <PageHeader>Pairings</PageHeader>
+      {pairings.length ? (
+        pairings.map(pairing => {
+          const { metadata } = pairing.state
+          console.log(pairing)
+
+          return (
+            <PairingCard
+              key={pairing.topic}
+              logo={metadata?.icons[0]}
+              url={metadata?.url}
+              name={metadata?.name}
+              onDelete={() => onDelete(pairing.topic)}
+            />
+          )
+        })
+      ) : (
+        <Text css={{ opacity: '0.5', textAlign: 'center', marginTop: '$20' }}>No pairings</Text>
+      )}
     </Fragment>
   )
 }
