240d8745628bb9461a1ee370b4fe81481d9834c9
Ben Kremer <ben@walletconnect.com>
2022-02-18 12:32:28 +0100

    feat: sets up react-dapp-v2-with-web3js

---
+++ dapps/react-dapp-v2-with-web3js/src/App.tsx
@@ -0,0 +1,338 @@
+import React, { useState } from "react";
+import { version } from "@walletconnect/client/package.json";
+import * as encoding from "@walletconnect/encoding";
+
+import Banner from "./components/Banner";
+import Blockchain from "./components/Blockchain";
+import Column from "./components/Column";
+import Header from "./components/Header";
+import Modal from "./components/Modal";
+import { DEFAULT_MAIN_CHAINS, DEFAULT_TEST_CHAINS } from "./constants";
+import { AccountAction, getLocalStorageTestnetFlag, setLocaleStorageTestnetFlag } from "./helpers";
+import Toggle from "./components/Toggle";
+import RequestModal from "./modals/RequestModal";
+import PingModal from "./modals/PingModal";
+import {
+  SAccounts,
+  SAccountsContainer,
+  SButtonContainer,
+  SContent,
+  SLanding,
+  SLayout,
+  SToggleContainer,
+} from "./components/app";
+import { useWalletConnectClient } from "./contexts/ClientContext";
+import { utils } from "ethers";
+
+interface IFormattedRpcResponse {
+  method: string;
+  address: string;
+  valid: boolean;
+  result: string;
+}
+
+export default function App() {
+  const [isTestnet, setIsTestnet] = useState(getLocalStorageTestnetFlag());
+  const [isRpcRequestPending, setIsRpcRequestPending] = useState(false);
+  const [rpcResult, setRpcResult] = useState<IFormattedRpcResponse | null>();
+
+  const [modal, setModal] = useState("");
+
+  const closeModal = () => setModal("");
+  const openPingModal = () => setModal("ping");
+  const openRequestModal = () => setModal("request");
+
+  // Initialize the WalletConnect client.
+  const {
+    client,
+    session,
+    disconnect,
+    chain,
+    accounts,
+    balances,
+    chainData,
+    isFetchingBalances,
+    isInitializing,
+    onEnable,
+    web3Provider,
+  } = useWalletConnectClient();
+
+  const ping = async () => {
+    if (typeof client === "undefined") {
+      throw new Error("WalletConnect Client is not initialized");
+    }
+
+    try {
+      setIsRpcRequestPending(true);
+      const _session = await client.session.get(client.session.topics[0]);
+      await client.session.ping(_session.topic);
+      setRpcResult({
+        address: "",
+        method: "ping",
+        valid: true,
+        result: "success",
+      });
+    } catch (error) {
+      console.error("RPC request failed:", error);
+    } finally {
+      setIsRpcRequestPending(false);
+    }
+  };
+
+  const onPing = async () => {
+    openPingModal();
+    await ping();
+  };
+
+  const testSendTransaction: () => Promise<IFormattedRpcResponse> = async () => {
+    if (!web3Provider) {
+      throw new Error("web3Provider not connected");
+    }
+
+    const [address] = await web3Provider.eth.getAccounts();
+
+    const tx = {
+      from: address,
+      to: address,
+      gasPrice: "20000000000",
+      value: "0",
+    };
+
+    const { transactionHash } = await web3Provider.eth.sendTransaction(tx);
+
+    return {
+      method: "eth_sendTransaction",
+      address,
+      valid: true,
+      result: transactionHash,
+    };
+  };
+
+  const testSignTransaction: () => Promise<IFormattedRpcResponse> = async () => {
+    if (!web3Provider) {
+      throw new Error("web3Provider not connected");
+    }
+
+    const [address] = await web3Provider.eth.getAccounts();
+
+    const tx = {
+      from: address,
+      to: address,
+      gasPrice: "20000000000",
+      value: "0",
+    };
+
+    // The return signature here is `RLPEncodedTransaction` but it actually returns as string (?).
+    const result = (await web3Provider.eth.signTransaction(tx)) as unknown as string;
+
+    return {
+      method: "eth_signTransaction",
+      address,
+      valid: true,
+      result,
+    };
+  };
+
+  const testSignMessage: () => Promise<IFormattedRpcResponse> = async () => {
+    if (!web3Provider) {
+      throw new Error("web3Provider not connected");
+    }
+    const msg = "hello world";
+    const hexMsg = encoding.utf8ToHex(msg, true);
+    const [address] = await web3Provider.eth.getAccounts();
+    const signature = await web3Provider.eth.personal.sign(hexMsg, address, "");
+    const valid = utils.verifyMessage(msg, signature) === address;
+    return {
+      method: "personal_sign",
+      address,
+      valid,
+      result: signature,
+    };
+  };
+
+  const testEthSign: () => Promise<IFormattedRpcResponse> = async () => {
+    if (!web3Provider) {
+      throw new Error("web3Provider not connected");
+    }
+    const msg = "hello world";
+    const hexMsg = encoding.utf8ToHex(msg, true);
+    const [address] = await web3Provider.eth.getAccounts();
+    const signature = await web3Provider.eth.sign(hexMsg, address);
+    const valid = utils.verifyMessage(msg, signature) === address;
+    return {
+      method: "eth_sign (standard)",
+      address,
+      valid,
+      result: signature,
+    };
+  };
+
+  // FIXME: need `web3.currentProvider.sendAsync` method for `signTypedData`, but EthereumProvider
+  // doesn't seem to have it implemented?
+  // e.g. https://docs.metamask.io/guide/signing-data.html#sign-typed-data-v4
+
+  // const testSignTypedData: () => Promise<IFormattedRpcResponse> = async () => {
+  //   if (!web3Provider) {
+  //     throw new Error("web3Provider not connected");
+  //   }
+
+  //   const typedData = {
+  //     types: {
+  //       Person: [
+  //         { name: "name", type: "string" },
+  //         { name: "wallet", type: "address" },
+  //       ],
+  //       Mail: [
+  //         { name: "from", type: "Person" },
+  //         { name: "to", type: "Person" },
+  //         { name: "contents", type: "string" },
+  //       ],
+  //     },
+  //     primaryType: "Mail",
+  //     domain: {
+  //       name: "Ether Mail",
+  //       version: "1",
+  //       chainId: 1,
+  //       verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+  //     },
+  //     message: {
+  //       from: {
+  //         name: "Cow",
+  //         wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+  //       },
+  //       to: {
+  //         name: "Bob",
+  //         wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+  //       },
+  //       contents: "Hello, Bob!",
+  //     },
+  //   };
+
+  //   const message = JSON.stringify(typedData);
+
+  //   const [address] = await web3Provider.eth.getAccounts();
+
+  //   // eth_signTypedData params
+  //   const params = [address, message];
+
+  //   // send message
+  //   const signature = await web3Provider.eth.signTypedData("eth_signTypedData", params);
+  //   const valid =
+  //     utils.verifyTypedData(typedData.domain, typedData.types, typedData.message, signature) ===
+  //     address;
+  //   return {
+  //     method: "eth_signTypedData",
+  //     address,
+  //     valid,
+  //     result: signature,
+  //   };
+  // };
+
+  const getEthereumActions = (): AccountAction[] => {
+    const wrapRpcRequest = (rpcRequest: () => Promise<IFormattedRpcResponse>) => async () => {
+      openRequestModal();
+      try {
+        setIsRpcRequestPending(true);
+        const result = await rpcRequest();
+        setRpcResult(result);
+      } catch (error) {
+        console.error("RPC request failed:", error);
+      } finally {
+        setIsRpcRequestPending(false);
+      }
+    };
+
+    return [
+      { method: "eth_sendTransaction", callback: wrapRpcRequest(testSendTransaction) },
+      { method: "eth_signTransaction", callback: wrapRpcRequest(testSignTransaction) },
+      { method: "personal_sign", callback: wrapRpcRequest(testSignMessage) },
+      { method: "eth_sign (standard)", callback: wrapRpcRequest(testEthSign) },
+      // { method: "eth_signTypedData", callback: wrapRpcRequest(testSignTypedData) },
+    ];
+  };
+
+  const getBlockchainActions = (chainId: string) => {
+    const [namespace] = chainId.split(":");
+    switch (namespace) {
+      case "eip155":
+        return getEthereumActions();
+      case "cosmos":
+        return [];
+      default:
+        break;
+    }
+  };
+
+  // Toggle between displaying testnet or mainnet chains as selection options.
+  const toggleTestnets = () => {
+    const nextIsTestnetState = !isTestnet;
+    setIsTestnet(nextIsTestnetState);
+    setLocaleStorageTestnetFlag(nextIsTestnetState);
+  };
+
+  // Renders the appropriate model for the given request that is currently in-flight.
+  const renderModal = () => {
+    switch (modal) {
+      case "request":
+        return <RequestModal pending={isRpcRequestPending} result={rpcResult} />;
+      case "ping":
+        return <PingModal pending={isRpcRequestPending} result={rpcResult} />;
+      default:
+        return null;
+    }
+  };
+
+  const renderContent = () => {
+    const chainOptions = isTestnet ? DEFAULT_TEST_CHAINS : DEFAULT_MAIN_CHAINS;
+    return !accounts.length && !Object.keys(balances).length ? (
+      <SLanding center>
+        <Banner />
+        <h6>
+          <span>{`Using v${version || "2.0.0-beta"}`}</span>
+        </h6>
+        <SButtonContainer>
+          <h6>Select an Ethereum chain:</h6>
+          <SToggleContainer>
+            <p>Testnets Only?</p>
+            <Toggle active={isTestnet} onClick={toggleTestnets} />
+          </SToggleContainer>
+          {chainOptions.map(chainId => (
+            <Blockchain key={chainId} chainId={chainId} chainData={chainData} onClick={onEnable} />
+          ))}
+        </SButtonContainer>
+      </SLanding>
+    ) : (
+      <SAccountsContainer>
+        <h3>Account</h3>
+        <SAccounts>
+          {accounts.map(account => {
+            return (
+              <Blockchain
+                key={account}
+                active={true}
+                chainData={chainData}
+                fetching={isFetchingBalances}
+                address={account}
+                chainId={chain}
+                balances={balances}
+                actions={getBlockchainActions(chain)}
+              />
+            );
+          })}
+        </SAccounts>
+      </SAccountsContainer>
+    );
+  };
+
+  return (
+    <SLayout>
+      <Column maxWidth={1000} spanHeight>
+        <Header ping={onPing} disconnect={disconnect} session={session} />
+        <SContent>{isInitializing ? "Loading..." : renderContent()}</SContent>
+      </Column>
+      <Modal show={!!modal} closeModal={closeModal}>
+        {renderModal()}
+      </Modal>
+    </SLayout>
+  );
+}
---
+++ dapps/react-dapp-v2-with-web3js/src/contexts/ClientContext.tsx
@@ -0,0 +1,299 @@
+import Client, { CLIENT_EVENTS } from "@walletconnect/client";
+import EthereumProvider from "@walletconnect/ethereum-provider";
+import { PairingTypes, SessionTypes } from "@walletconnect/types";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import Web3 from "web3";
+import { apiGetChainNamespace, ChainsMap } from "caip-api";
+
+import {
+  DEFAULT_INFURA_ID,
+  DEFAULT_LOGGER,
+  DEFAULT_PROJECT_ID,
+  DEFAULT_RELAY_URL,
+} from "../constants";
+import { ChainNamespaces, getAllChainNamespaces } from "../helpers";
+import { utils } from "ethers";
+
+/**
+ * Types
+ */
+interface IContext {
+  client: Client | undefined;
+  session: SessionTypes.Created | undefined;
+  disconnect: () => Promise<void>;
+  isInitializing: boolean;
+  chain: string;
+  pairings: string[];
+  accounts: string[];
+  balances: { symbol: string; balance: string }[];
+  isFetchingBalances: boolean;
+  chainData: ChainNamespaces;
+  onEnable: (chainId: string) => Promise<void>;
+  web3Provider?: Web3;
+}
+
+/**
+ * Context
+ */
+export const ClientContext = createContext<IContext>({} as IContext);
+
+/**
+ * Provider
+ */
+export function ClientContextProvider({ children }: { children: ReactNode | ReactNode[] }) {
+  const [client, setClient] = useState<Client>();
+  const [pairings, setPairings] = useState<string[]>([]);
+  const [session, setSession] = useState<SessionTypes.Created>();
+
+  const [ethereumProvider, setEthereumProvider] = useState<EthereumProvider>();
+  const [web3Provider, setWeb3Provider] = useState<Web3>();
+
+  const [isFetchingBalances, setIsFetchingBalances] = useState(false);
+  const [isInitializing, setIsInitializing] = useState(false);
+  const [hasCheckedPersistedSession, setHasCheckedPersistedSession] = useState(false);
+
+  const [balances, setBalances] = useState<{ symbol: string; balance: string }[]>([]);
+  const [accounts, setAccounts] = useState<string[]>([]);
+  const [chainData, setChainData] = useState<ChainNamespaces>({});
+  const [chain, setChain] = useState<string>("");
+
+  const resetApp = () => {
+    setPairings([]);
+    setSession(undefined);
+    setBalances([]);
+    setAccounts([]);
+    setChain("");
+  };
+
+  const loadChainData = async () => {
+    const namespaces = getAllChainNamespaces();
+    const chainData: ChainNamespaces = {};
+    await Promise.all(
+      namespaces.map(async namespace => {
+        let chains: ChainsMap | undefined;
+        try {
+          chains = await apiGetChainNamespace(namespace);
+        } catch (e) {
+          // ignore error
+        }
+        if (typeof chains !== "undefined") {
+          chainData[namespace] = chains;
+        }
+      }),
+    );
+    setChainData(chainData);
+  };
+
+  const disconnect = useCallback(async () => {
+    if (typeof ethereumProvider === "undefined") {
+      throw new Error("ethereumProvider is not initialized");
+    }
+    await ethereumProvider.disconnect();
+  }, [ethereumProvider]);
+
+  const _subscribeToClientEvents = useCallback(async (_client: Client) => {
+    if (typeof _client === "undefined") {
+      throw new Error("WalletConnect is not initialized");
+    }
+
+    _client.on(CLIENT_EVENTS.pairing.proposal, async (proposal: PairingTypes.Proposal) => {
+      const { uri } = proposal.signal.params;
+      console.log("EVENT", "QR Code Modal open");
+      QRCodeModal.open(uri, () => {
+        console.log("EVENT", "QR Code Modal closed");
+      });
+    });
+
+    _client.on(CLIENT_EVENTS.pairing.created, async () => {
+      setPairings(_client.pairing.topics);
+    });
+
+    _client.on(CLIENT_EVENTS.session.deleted, () => {
+      console.log("EVENT", "session_deleted");
+      resetApp();
+    });
+  }, []);
+
+  const createClient = useCallback(async () => {
+    try {
+      setIsInitializing(true);
+
+      const _client = await Client.init({
+        logger: DEFAULT_LOGGER,
+        relayUrl: DEFAULT_RELAY_URL,
+        projectId: DEFAULT_PROJECT_ID,
+      });
+
+      setClient(_client);
+      await _subscribeToClientEvents(_client);
+    } catch (err) {
+      throw err;
+    } finally {
+      setIsInitializing(false);
+    }
+  }, [_subscribeToClientEvents]);
+
+  const onEnable = useCallback(
+    async (caipChainId: string) => {
+      if (!client) {
+        throw new ReferenceError("WalletConnect Client is not initialized.");
+      }
+
+      const chainId = caipChainId.split(":").pop();
+
+      console.log("Enabling EthereumProvider for chainId: ", chainId);
+
+      const customRpcs = Object.keys(chainData.eip155).reduce(
+        (rpcs: Record<string, string>, chainId) => {
+          rpcs[chainId] = chainData.eip155[chainId].rpc[0];
+          return rpcs;
+        },
+        {},
+      );
+
+      //  Create WalletConnect Provider
+      const ethereumProvider = new EthereumProvider({
+        chainId: Number(chainId),
+        rpc: {
+          infuraId: DEFAULT_INFURA_ID,
+          custom: customRpcs,
+        },
+        client,
+      });
+      // FIXME:
+      // Type 'EthereumProvider' is missing the following properties from type 'WebsocketProvider':
+      // isConnecting, requestQueue, responseQueue, connection, and 5 more.ts(2345)
+      // @ts-expect-error
+      const web3Provider = new Web3(ethereumProvider);
+
+      console.log(ethereumProvider);
+      console.log(web3Provider);
+
+      setEthereumProvider(ethereumProvider);
+      setWeb3Provider(web3Provider);
+
+      const _accounts = await ethereumProvider.enable();
+      const _session = await client.session.get(client.session.topics[0]);
+
+      setAccounts(_accounts);
+      setSession(_session);
+      setChain(caipChainId);
+
+      try {
+        setIsFetchingBalances(true);
+        const _balances = await Promise.all(
+          _accounts.map(async account => {
+            const balance = await web3Provider.eth.getBalance(account);
+            return { symbol: "ETH", balance: utils.formatEther(balance) };
+          }),
+        );
+        setBalances(_balances);
+      } catch (error: any) {
+        throw new Error(error);
+      } finally {
+        setIsFetchingBalances(false);
+      }
+
+      QRCodeModal.close();
+    },
+    [client, chainData.eip155],
+  );
+
+  const _checkForPersistedSession = useCallback(
+    async (_client: Client) => {
+      if (typeof _client === "undefined") {
+        throw new Error("WalletConnect is not initialized");
+      }
+      // populates existing pairings to state
+      setPairings(_client.pairing.topics);
+      if (typeof session !== "undefined") return;
+      // populates existing session to state (assume only the top one)
+      if (_client.session.topics.length) {
+        const _session = await _client.session.get(_client.session.topics[0]);
+        const [namespace, chainId] = _session.state.accounts[0].split(":");
+        const caipChainId = `${namespace}:${chainId}`;
+        onEnable(caipChainId);
+      }
+    },
+    [session, onEnable],
+  );
+
+  useEffect(() => {
+    loadChainData();
+  }, []);
+
+  useEffect(() => {
+    if (!client) {
+      createClient();
+    }
+  }, [client, createClient]);
+
+  useEffect(() => {
+    const getPersistedSession = async () => {
+      if (client && !hasCheckedPersistedSession) {
+        await _checkForPersistedSession(client);
+        setHasCheckedPersistedSession(true);
+      }
+    };
+
+    getPersistedSession();
+  }, [client, _checkForPersistedSession, hasCheckedPersistedSession]);
+
+  const value = useMemo(
+    () => ({
+      pairings,
+      isInitializing,
+      balances,
+      isFetchingBalances,
+      accounts,
+      chain,
+      client,
+      session,
+      disconnect,
+      chainData,
+      onEnable,
+      web3Provider,
+    }),
+    [
+      pairings,
+      isInitializing,
+      balances,
+      isFetchingBalances,
+      accounts,
+      chain,
+      client,
+      session,
+      disconnect,
+      chainData,
+      onEnable,
+      web3Provider,
+    ],
+  );
+
+  return (
+    <ClientContext.Provider
+      value={{
+        ...value,
+      }}
+    >
+      {children}
+    </ClientContext.Provider>
+  );
+}
+
+export function useWalletConnectClient() {
+  const context = useContext(ClientContext);
+  if (context === undefined) {
+    throw new Error("useWalletConnectClient must be used within a ClientContextProvider");
+  }
+  return context;
+}
