8d7b4ab17c8c4eac6cebf6ce336f80abb9aa346e
Ilja <idaderko@gmail.com>
2022-03-07 12:18:38 +0200

    prep for canary release flow

--- wallets/react-wallet-v2/src/utils/WalletConnectUtil.ts
+++ wallets/react-wallet-v2/src/utils/WalletConnectUtil.ts
@@ -6,7 +6,7 @@ export async function createWalletConnectClient() {
   walletConnectClient = await WalletConnectClient.init({
     controller: true,
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    relayUrl: 'wss://relay.walletconnect.com',
+    relayUrl: process.env.NEXT_PUBLIC_RELAY_URL ?? 'wss://relay.walletconnect.com',
     metadata: {
       name: 'React Wallet',
       description: 'React Wallet for WalletConnect',
