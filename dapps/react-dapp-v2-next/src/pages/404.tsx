77795bf72a0eb5e830a05852b104777021273b77
Celine Sarafa <celinesarafa@gmail.com>
2022-08-05 18:01:56 +0300

    Update nextjs structure and prep for static

---
+++ dapps/react-dapp-v2-next/src/pages/404.tsx
@@ -0,0 +1,5 @@
+export default function FourOhFour() {
+	return (
+		<h1>404 Page Not Found</h1>
+	)
+}
---
+++ dapps/react-dapp-v2-next/src/pages/_error.tsx
@@ -0,0 +1,7 @@
+export default function Error() {
+	return (
+		<div>
+			An error as occured
+		</div>
+	)
+}
