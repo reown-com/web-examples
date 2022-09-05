aedddbcbdca8c9ec6582484991a13e0ff174252e
Celine Sarafa <celinesarafa@gmail.com>
2022-09-05 10:50:21 +0300

    Remove session from nav for auth (#51)
    * Removed session from nav
    
    * Add note that you can copy the qrcode
    
    * Moving WC icon to the right

--- wallets/react-wallet-auth/src/components/Navigation.tsx
+++ wallets/react-wallet-auth/src/components/Navigation.tsx
@@ -11,9 +11,15 @@ export default function Navigation() {
         </a>
       </Link>
 
-      <Link href="/sessions" passHref>
+      <Link href="/pairings" passHref>
         <a className="navLink">
-          <Image alt="sessions icon" src="/icons/sessions-icon.svg" width={27} height={27} />
+          <Image alt="pairings icon" src="/icons/pairings-icon.svg" width={25} height={25} />
+        </a>
+      </Link>
+
+      <Link href="/settings" passHref>
+        <a className="navLink">
+          <Image alt="settings icon" src="/icons/settings-icon.svg" width={27} height={27} />
         </a>
       </Link>
 
@@ -32,18 +38,6 @@ export default function Navigation() {
               />
             }
           />
-        </a>
-      </Link>
-
-      <Link href="/pairings" passHref>
-        <a className="navLink">
-          <Image alt="pairings icon" src="/icons/pairings-icon.svg" width={25} height={25} />
-        </a>
-      </Link>
-
-      <Link href="/settings" passHref>
-        <a className="navLink">
-          <Image alt="settings icon" src="/icons/settings-icon.svg" width={27} height={27} />
         </a>
       </Link>
     </Row>
