9c9e4601c3035cc0c6e063fc17b0f126f06f9ece
Ben Kremer <contact@bkrem.dev>
2022-10-06 13:39:10 +0200

    fix(wallet): fix rgb colours for Optimism list item

--- wallets/react-wallet-v2/src/data/EIP155Data.ts
+++ wallets/react-wallet-v2/src/data/EIP155Data.ts
@@ -19,13 +19,6 @@ export const EIP155_MAINNET_CHAINS = {
     rgb: '99, 125, 234',
     rpc: 'https://cloudflare-eth.com/'
   },
-  'eip155:10': {
-    chainId: 10,
-    name: 'Optimism',
-    logo: '/chain-logos/eip155-10.png',
-    rgb: '99, 125, 234',
-    rpc: 'https://mainnet.optimism.io'
-  },
   'eip155:43114': {
     chainId: 43114,
     name: 'Avalanche C-Chain',
@@ -39,6 +32,13 @@ export const EIP155_MAINNET_CHAINS = {
     logo: '/chain-logos/eip155-137.png',
     rgb: '130, 71, 229',
     rpc: 'https://polygon-rpc.com/'
+  },
+  'eip155:10': {
+    chainId: 10,
+    name: 'Optimism',
+    logo: '/chain-logos/eip155-10.png',
+    rgb: '235, 0, 25',
+    rpc: 'https://mainnet.optimism.io'
   }
 }
 
@@ -49,13 +49,6 @@ export const EIP155_TEST_CHAINS = {
     logo: '/chain-logos/eip155-1.png',
     rgb: '99, 125, 234',
     rpc: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
-  },
-  'eip155:420': {
-    chainId: 420,
-    name: 'Optimism Goerli',
-    logo: '/chain-logos/eip155-10.png',
-    rgb: '99, 125, 234',
-    rpc: 'https://goerli.optimism.io'
   },
   'eip155:43113': {
     chainId: 43113,
@@ -70,6 +63,13 @@ export const EIP155_TEST_CHAINS = {
     logo: '/chain-logos/eip155-137.png',
     rgb: '130, 71, 229',
     rpc: 'https://matic-mumbai.chainstacklabs.com'
+  },
+  'eip155:420': {
+    chainId: 420,
+    name: 'Optimism Goerli',
+    logo: '/chain-logos/eip155-10.png',
+    rgb: '235, 0, 25',
+    rpc: 'https://goerli.optimism.io'
   }
 }
 
