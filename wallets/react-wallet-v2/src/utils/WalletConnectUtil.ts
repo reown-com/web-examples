9743a224dea291754e7a274942934e19f8b842e5
Hanwoong <hanationbear@gmail.com>
2022-11-01 14:50:50 +0900

    Update WalletConnectUtil.ts
    fix name NEXT_PUBLIC_RELAY_URL

--- wallets/react-wallet-v2/src/utils/WalletConnectUtil.ts
+++ wallets/react-wallet-v2/src/utils/WalletConnectUtil.ts
@@ -6,7 +6,7 @@ export async function createSignClient(relayerRegionURL: string) {
   signClient = await SignClient.init({
     logger: 'debug',
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    relayUrl: relayerRegionURL ?? process.env.NEXT_PUBLIC_RELAYER_URL,
+    relayUrl: relayerRegionURL ?? process.env.NEXT_PUBLIC_RELAY_URL,
     metadata: {
       name: 'React Wallet',
       description: 'React Wallet for WalletConnect',
