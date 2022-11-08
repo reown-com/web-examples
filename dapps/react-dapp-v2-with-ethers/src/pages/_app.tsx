bc7712780a101f0152c20ce4eca4d8d117d8eaa7
Gancho Radkov <ganchoradkov93@gmail.com>
2022-11-08 10:06:10 +0200

    chore: to trigger deployment

--- dapps/react-dapp-v2-with-ethers/src/pages/_app.tsx
+++ dapps/react-dapp-v2-with-ethers/src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { globalStyle } from "../styles";
 const GlobalStyle = createGlobalStyle`
   ${globalStyle}
 `;
-
+console.log("trigger deploy");
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -18,5 +18,4 @@ function MyApp({ Component, pageProps }: AppProps) {
     </>
   );
 }
-
 export default MyApp;
