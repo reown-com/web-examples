4f8d26dda4c7ed9735e2a3fca8b7808e9f5a0851
Ben Kremer <ben@walletconnect.com>
2023-02-02 20:03:56 +0700

    chore: update canonical dapp/wallet to `@walletconnect/*@2.3.3` (#110)
    * chore: update @walletconnect/* deps to 2.3.3
    
    * fix(types): clean up `namespace.extension` references

--- wallets/react-wallet-v2/src/components/SessionProposalChainCard.tsx
+++ wallets/react-wallet-v2/src/components/SessionProposalChainCard.tsx
@@ -40,18 +40,6 @@ export default function SessionProposalChainCard({ requiredNamespace }: IProps) 
   return (
     <Fragment>
       {requiredNamespace.chains.map(chainId => {
-        const extensionMethods: ProposalTypes.RequiredNamespace['methods'] = []
-        const extensionEvents: ProposalTypes.RequiredNamespace['events'] = []
-
-        requiredNamespace.extension?.map(({ chains, methods, events }) => {
-          if (chains.includes(chainId)) {
-            extensionMethods.push(...methods)
-            extensionEvents.push(...events)
-          }
-        })
-
-        const allMethods = [...requiredNamespace.methods, ...extensionMethods]
-        const allEvents = [...requiredNamespace.events, ...extensionEvents]
         // @ts-expect-error
         const rgb = CHAIN_METADATA[chainId]?.rgb
 
@@ -63,13 +51,17 @@ export default function SessionProposalChainCard({ requiredNamespace }: IProps) 
             <Row>
               <Col>
                 <Text h6>Methods</Text>
-                <Text color="$gray300">{allMethods.length ? allMethods.join(', ') : '-'}</Text>
+                <Text color="$gray300">
+                  {requiredNamespace.methods.length ? requiredNamespace.methods.join(', ') : '-'}
+                </Text>
               </Col>
             </Row>
             <Row css={{ marginTop: '$5' }}>
               <Col>
                 <Text h6>Events</Text>
-                <Text color="$gray300">{allEvents.length ? allEvents.join(', ') : '-'}</Text>
+                <Text color="$gray300">
+                  {requiredNamespace.events.length ? requiredNamespace.events.join(', ') : '-'}
+                </Text>
               </Col>
             </Row>
           </ChainCard>
