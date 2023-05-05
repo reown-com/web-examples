c79ba13d6b9e1510fc989a064352efe14c0cb132
Jonathan Conn <jonathansconn@gmail.com>
2023-05-04 20:11:27 -0400

    Fixed the chain changed event by updating the session if the chainId namespace does not currently exist, then emitting the correct chainChanged event on the same session

--- wallets/react-wallet-v2/src/utils/WalletConnectUtil.ts
+++ wallets/react-wallet-v2/src/utils/WalletConnectUtil.ts
@@ -16,45 +16,48 @@ export async function createSignClient(relayerRegionURL: string) {
 }
 
 export async function updateSignClientChainId(chainId: string, address: string) {
-  const sessions = signClient.session.getAll()
-  for (const session of sessions) {
-    console.log(`chainId: ${chainId}`)
-    signClient.emit({
-      topic: session.topic,
-      event: {
-        name: 'chainChanged',
-        data: [address]
-      },
-      chainId: `eip155:1`
-    })
+  // get most recent session
+  const session = signClient.session.getAll()[0]
+  if (!session) return
 
-    // const fullChainName = getFullChainName(chainId)
-    // const chain = fullChainName.substring(0, fullChainName.indexOf(':'))
-    // const accounts = session.namespaces[chain] ? session.namespaces[chain].accounts : []
-    // const new_account = `${fullChainName}:${address}`
+  // if chainId does not exist in session, an update is required first
+  if (!session.namespaces[chainId]) {
+    const newNamespace = {
+      [chainId]: {
+        accounts: [`${chainId}:${address}`],
+        methods: [
+          'eth_sendTransaction',
+          'eth_signTransaction',
+          'eth_sign',
+          'personal_sign',
+          'eth_signTypedData'
+        ],
+        events: ['chainChanged', 'accountsChanged']
+      }
+    }
+    try {
+      // need to wait for update to finish before emit
+      await signClient.update({
+        topic: session.topic,
+        namespaces: { ...session.namespaces, ...newNamespace }
+      })
+    } catch (err: unknown) {
+      console.error(`Failed to update session: ${err}`)
+    }
+  }
 
-    // // ensures no dup namespace accounts get added
-    // if (!accounts.includes(new_account)) {
-    //   accounts.push(new_account)
-    // }
+  const payload = {
+    topic: session.topic,
+    event: {
+      name: 'chainChanged',
+      data: [address]
+    },
+    chainId
+  }
 
-    // const newNamespace = {
-    //   [chain]: {
-    //     accounts: accounts,
-    //     methods: [
-    //       'eth_sendTransaction',
-    //       'eth_signTransaction',
-    //       'eth_sign',
-    //       'personal_sign',
-    //       'eth_signTypedData'
-    //     ],
-    //     events: ['chainChanged', 'accountsChanged']
-    //   }
-    // }
-
-    // signClient.update({
-    //   topic: session.topic,
-    //   namespaces: session.namespaces ? { ...session.namespaces, ...newNamespace } : newNamespace
-    // })
+  try {
+    signClient.emit(payload)
+  } catch (err: unknown) {
+    console.error(`Failed to emit chainChanged event: ${err}`)
   }
 }
