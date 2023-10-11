337984c51339da6571f3a7af28c345ea704eb011
Radu Mojic <radu.mojic@multiversx.com>
2023-10-11 19:48:42 +0300

    [+] added the mvx_cancelAction method for the MultiversX chain

--- wallets/react-wallet-v2/src/data/MultiversxData.ts
+++ wallets/react-wallet-v2/src/data/MultiversxData.ts
@@ -47,5 +47,6 @@ export const MULTIVERSX_SIGNING_METHODS = {
   MULTIVERSX_SIGN_TRANSACTIONS: 'mvx_signTransactions',
   MULTIVERSX_SIGN_MESSAGE: 'mvx_signMessage',
   MULTIVERSX_SIGN_LOGIN_TOKEN: 'mvx_signLoginToken',
-  MULTIVERSX_SIGN_NATIVE_AUTH_TOKEN: 'mvx_signNativeAuthToken'
+  MULTIVERSX_SIGN_NATIVE_AUTH_TOKEN: 'mvx_signNativeAuthToken',
+  MULTIVERSX_CANCEL_ACTION: 'mvx_cancelAction'
 }
