7b01cb1bc9950ffc611d3b02c09fa7ec28c3bfef
Eloi Manuel <eloi.manvie@gmail.com>
2023-10-27 18:49:55 +0200

    Added Sepolia - Required by testnets.opensea.io

--- wallets/react-wallet-v2/src/data/EIP155Data.ts
+++ wallets/react-wallet-v2/src/data/EIP155Data.ts
@@ -63,6 +63,14 @@ export const EIP155_TEST_CHAINS = {
     rpc: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
     namespace: 'eip155'
   },
+  'eip155:11155111': {
+    chainId: 11155111,
+    name: 'Ethereum Sepolia',
+    logo: '/chain-logos/eip155-1.png',
+    rgb: '99, 125, 234',
+    rpc: 'https://sepolia.rpc.thirdweb.com',
+    namespace: 'eip155'
+  },
   'eip155:43113': {
     chainId: 43113,
     name: 'Avalanche Fuji',
