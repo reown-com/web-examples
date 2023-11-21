44922fae5b1b47745eac824d9ff73bcaf7bc4443
0xAsimetriq <idaderko@gmail.com>
2023-11-21 12:12:36 +0100

    display smart account

--- wallets/react-wallet-v2/src/lib/SmartAccountLib.ts
+++ wallets/react-wallet-v2/src/lib/SmartAccountLib.ts
@@ -30,7 +30,7 @@ export const bundlerClient = createPimlicoBundlerClient({
 })
 
 // -- Smart Account --------------------------------------------------------------------------------
-export async function getOrCreateSmartAccount(signerPrivateKey: `0x${string}`) {
+export async function getSmartAccount(signerPrivateKey: `0x${string}`) {
   // Step 1: Create smart account details and client
   const smartAccount = await privateKeyToSafeSmartAccount(publicClient, {
     privateKey: signerPrivateKey,
@@ -49,40 +49,45 @@ export async function getOrCreateSmartAccount(signerPrivateKey: `0x${string}`) {
   const isDeployed = Boolean(bytecode)
   console.log(`Is Smart Account Deployed? ${isDeployed}`)
 
-  // Step 3: If not deployed, prefund smart account from signer
-  if (!isDeployed) {
-    // Step 3.1: Create signer account and client from private key
-    const signerAccount = privateKeyToAccount(signerPrivateKey)
-    const signerAccountViemClient = createWalletClient({
-      account: signerAccount,
-      chain: sepolia,
-      transport: walletConnectTransport
-    })
+  return {
+    smartAccountViemClient,
+    isDeployed,
+    deploy: async () => {
+      // Step 3: If not deployed, prefund smart account from signer
+      if (!isDeployed) {
+        // Step 3.1: Create signer account and client from private key
+        const signerAccount = privateKeyToAccount(signerPrivateKey)
+        const signerAccountViemClient = createWalletClient({
+          account: signerAccount,
+          chain: sepolia,
+          transport: walletConnectTransport
+        })
 
-    // Step 3.2: Precalculate necessary prefund amount
+        // Step 3.2: Precalculate necessary prefund amount
+        const data = await bundlerClient.getUserOperationGasPrice()
 
-    // prepareUserOperationRequest(smartAccountViemClient, {
-    // })
+        // prepareUserOperationRequest(smartAccountViemClient, {
+        // })
 
-    // const { maxFeePerGas, preVerificationGas, verificationGasLimit, callGasLimit } =
-    //   smartAccountClient.userOp
-    // const prefundAmount =
-    //   maxFeePerGas * (preVerificationGas + 3 * verificationGasLimit + callGasLimit)
+        // const { maxFeePerGas, preVerificationGas, verificationGasLimit, callGasLimit } =
+        //   smartAccountClient.userOp
+        // const prefundAmount =
+        //   maxFeePerGas * (preVerificationGas + 3 * verificationGasLimit + callGasLimit)
 
-    // Step 3.3: Send prefund transaction from signer to smart account
-    const { fast } = await bundlerClient.getUserOperationGasPrice()
-    const prefundHash = await signerAccountViemClient.sendTransaction({
-      to: smartAccountViemClient.account.address,
-      value: 0n,
-      maxFeePerGas: fast.maxFeePerGas,
-      maxPriorityFeePerGas: fast.maxPriorityFeePerGas
-    })
-    console.log(`Prefunding Smart Account: ${prefundHash}`)
-    await publicClient.waitForTransactionReceipt({ hash: prefundHash })
-    console.log(`Prefunding Success`)
+        // Step 3.3: Send prefund transaction from signer to smart account
+        const { fast } = await bundlerClient.getUserOperationGasPrice()
+        const prefundHash = await signerAccountViemClient.sendTransaction({
+          to: smartAccountViemClient.account.address,
+          value: 50000000000000000n,
+          maxFeePerGas: fast.maxFeePerGas,
+          maxPriorityFeePerGas: fast.maxPriorityFeePerGas
+        })
+        console.log(`Prefunding Smart Account: ${prefundHash}`)
+        await publicClient.waitForTransactionReceipt({ hash: prefundHash })
+        console.log(`Prefunding Success`)
+      }
+    }
   }
-
-  return { smartAccountViemClient }
 }
 
 // export async function sendTestTransaction(smartAccountViemClient: WalletClient) {
