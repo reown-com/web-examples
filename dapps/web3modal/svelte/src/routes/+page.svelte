b887c0f3fb3b5a38f4bf72cc5b8d82c73f3855c1
Glitch <glitch.txs@gmail.com>
2023-12-13 15:51:19 -0300

    feat/svelte

---
+++ dapps/web3modal/svelte/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { account, network } from '$lib/wagmi';
+</script>
+
+<div class="container" >
+  <h1>Welcome to SvelteKit & Web3Modal</h1>
+  <div>
+    <div>user address: {$account.address}</div>
+    <div>Connected chain: {$network.chain?.name}</div>
+  </div>
+  <w3m-button/>
+</div>
+
+<style>
+  :global(*){
+    margin: 0;
+    padding: 0;
+  }
+  :global(body, html){
+    width: 100%;
+    min-height: 100vh;
+  }
+
+  .container{
+    padding: 20px;
+    text-align: center;
+
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 100px;
+  }
+</style>
