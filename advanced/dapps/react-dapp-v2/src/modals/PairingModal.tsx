0fe3a62c634d9172dc6b7f1a13fc4c6ef5501d1a
Gancho Radkov <ganchoradkov@gmail.com>
2024-01-11 10:15:21 +0200

    feat: adds loaders on disconnect & when proposing session on known pairing topic

--- advanced/dapps/react-dapp-v2/src/modals/PairingModal.tsx
+++ advanced/dapps/react-dapp-v2/src/modals/PairingModal.tsx
@@ -7,6 +7,8 @@ import Pairing from "../components/Pairing";
 import { STable } from "../components/shared";
 
 import { SModalContainer, SModalTitle } from "./shared";
+import LoaderModal from "./LoaderModal";
+import toast from "react-hot-toast";
 
 interface PairingModalProps {
   pairings: PairingTypes.Struct[];
@@ -15,7 +17,28 @@ interface PairingModalProps {
 
 const PairingModal = (props: PairingModalProps) => {
   const { pairings, connect } = props;
-  return (
+  const [pairing, setPairing] = React.useState<PairingTypes.Struct>();
+
+  const onConnect = React.useCallback(
+    async (pairing: PairingTypes.Struct) => {
+      try {
+        setPairing(pairing);
+        await connect({ topic: pairing.topic });
+      } catch (error) {
+        toast.error((error as Error).message, {
+          position: "bottom-left",
+        });
+        setPairing(undefined);
+      }
+    },
+    [connect]
+  );
+  return pairing ? (
+    <LoaderModal
+      title={`Connecting to ${pairing?.peerMetadata?.name}`}
+      text="Open your wallet to approve the connection request"
+    />
+  ) : (
     <SModalContainer>
       <SModalTitle>{"Select available pairing or create new one"}</SModalTitle>
       <STable>
@@ -23,7 +46,7 @@ const PairingModal = (props: PairingModalProps) => {
           <Pairing
             key={pairing.topic}
             pairing={pairing}
-            onClick={() => connect({ topic: pairing.topic })}
+            onClick={() => onConnect(pairing)}
           />
         ))}
       </STable>
