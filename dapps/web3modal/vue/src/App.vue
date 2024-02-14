54e0c161324c1d1fecc3b30cc0db92333966d660
AbSattar <mdabdullsattar@gmail.com>
2024-02-15 03:28:13 +0530

    default mode implemented in app.vue

--- dapps/web3modal/vue/src/App.vue
+++ dapps/web3modal/vue/src/App.vue
@@ -1,26 +1,41 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+  import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { mainnet, arbitrum } from 'viem/chains'
+  import { reconnect } from '@wagmi/core'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  // 1. Define constants
+  const projectId = 'YOUR_PROJECT_ID'
+
+  // 2. Create wagmiConfig
+  const metadata = {
+    name: 'Web3Modal',
+    description: 'Web3Modal Example',
+    url: 'https://web3modal.com', // origin must match your domain & subdomain
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
   }
-}
+
+  const chains = [mainnet, arbitrum]
+  const config = defaultWagmiConfig({
+    chains, // required
+    projectId, // required
+    metadata, // required
+    enableWalletConnect: true, // Optional - true by default
+    enableInjected: true, // Optional - true by default
+    enableEIP6963: true, // Optional - true by default
+    enableCoinbase: true // Optional - true by default
+  })
+
+  reconnect(config)
+  // 3. Create modal
+  createWeb3Modal({
+    wagmiConfig: config,
+    projectId,
+    enableAnalytics: true // Optional - defaults to your Cloud configuration
+  })
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <w3m-button />
+</template>
+
