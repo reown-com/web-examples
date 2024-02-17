63de72ca2f572f464ea62254e49cfa8e26c4856d
0xPr0f <0xprofoz@gmail.com>
2024-02-17 02:00:13 +0100

    renovate

--- dapps/web3modal/react/src/main.tsx
+++ dapps/web3modal/react/src/main.tsx
@@ -1,36 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createWeb3Modal } from "@web3modal/wagmi/react";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createWeb3Modal } from '@web3modal/wagmi/react'
 
-import { http, createConfig, WagmiProvider } from "wagmi";
-import { mainnet, arbitrum } from "viem/chains";
-import { walletConnect, coinbaseWallet } from "wagmi/connectors";
+import { http, createConfig, WagmiProvider } from 'wagmi'
+import { mainnet, arbitrum, sepolia } from 'viem/chains'
+import { walletConnect, coinbaseWallet } from 'wagmi/connectors'
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Contracts from './contract'
 
 // 0. Setup queryClient
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 // 1. Get projectId at https://cloud.walletconnect.com
-const projectId = import.meta.env.VITE_PROJECT_ID;
-if (!projectId) throw new Error("Project ID is undefined");
+const projectId = import.meta.env.VITE_PROJECT_ID
+if (!projectId) throw new Error('Project ID is undefined')
 
 // 2. Create wagmiConfig
 const metadata = {
-  name: "Web3Modal",
-  description: "Web3Modal Example",
-  url: "https://web3modal.com",
-  icons: ["https://avatars.githubusercontent.com/u/37784886"],
-};
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+}
 
 // Define chains
-const chains = [mainnet, arbitrum] as const
+const chains = [mainnet, sepolia] as const
 
 const wagmiConfig = createConfig({
   chains, // Use the defined chains here
   transports: {
     [mainnet.id]: http(),
-    [arbitrum.id]: http(),
+    [sepolia.id]: http(),
   },
   connectors: [
     walletConnect({ projectId, metadata, showQrModal: false }),
@@ -39,17 +40,18 @@ const wagmiConfig = createConfig({
       appLogoUrl: metadata.icons[0],
     }),
   ],
-});
+})
 
 // 3. Create modal
-createWeb3Modal({ wagmiConfig, projectId });
+createWeb3Modal({ wagmiConfig, projectId })
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <w3m-button />
+        <Contracts />
       </QueryClientProvider>
     </WagmiProvider>
   </React.StrictMode>
-);
+)
