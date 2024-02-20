60c02d16791b81266c18cc26493267505e7885bf
Glitch <glitch.txs@gmail.com>
2024-02-20 18:18:01 -0300

    style form

--- dapps/web3modal/svelte/src/partials/CustomForm.svelte
+++ dapps/web3modal/svelte/src/partials/CustomForm.svelte
@@ -8,9 +8,7 @@
 	}
 </script>
 
-
-
-<div>Add a Custom Wallet to Web3Modal</div>
+<div id="title">Add a Custom Wallet to Web3Modal</div>
 <form on:submit|preventDefault={handleSubmit} >
   <label>
 		Id
@@ -48,10 +46,81 @@
 		Play Store (Optional)
 		<input name="play_store" type="url" bind:value={$customWallet.play_store}>
 	</label>
-  <button type="submit" >Apply</button>
+	<button type="submit" >Apply</button>
 </form>
 
 
 <style>
+	#title{
+		text-align: center;
+		font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI',
+			Roboto, Ubuntu, 'Helvetica Neue', sans-serif;
+		font-weight: 600;
+		font-size: 24px;	
+	}
 
+	form{
+		position: relative;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+
+		width: 100%;
+		gap: 40px;
+		padding-bottom: 100px;
+	}
+
+	label{
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: 10px;
+		width: 300px;
+
+		font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI',
+			Roboto, Ubuntu, 'Helvetica Neue', sans-serif;
+		font-weight: 400;
+		font-size: 14px;
+	}
+
+	input {
+		border-radius: 10px;
+		outline: 2px solid rgba(40, 144, 255, 0.8);
+		border: 0;
+		background-color: #e2e2e2;
+		outline-offset: 3px;
+		padding: 10px 1rem;
+		transition: 0.25s;
+		font-size: 15px;
+	}
+
+	input:focus {
+		outline-offset: 5px;
+		outline: 2px solid #2890ff;
+		background-color: #fff
+	}
+
+	button {
+		position: absolute;
+		bottom: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		background: #FFFFFF;
+		border: 0 solid #E2E8F0;
+		box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+		box-sizing: border-box;
+		color: #1A202C;
+		font-size: 1rem;
+		font-weight: 700;
+		height: 56px;
+		text-decoration: none;
+		border-radius: 8px;
+		cursor: pointer;
+		width: 90px;
+		user-select: none;
+		-webkit-user-select: none;
+		touch-action: manipulation;
+	}
 </style>
