78268394980c1e4dd343c461dd4b6f7724ba194b
Glitch <66949816+glitch-txs@users.noreply.github.com>
2024-02-26 13:10:45 -0300

    vue sample (#483) (#486)
    * vue sample (#483)
    
    * initilaized vue
    
    * default mode implemented in app.vue
    
    * envexample added for projectID
    
    * removed empty space and tested dapp
    
    * deleted helloworld example component
    
    * pnpm
    
    * Update App.vue
    
    ---------
    
    Co-authored-by: Mohammed Abdul Sattar <93962265+mabsattar@users.noreply.github.com>

---
+++ dapps/web3modal/vue/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+  import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/vue'
+  import { mainnet, arbitrum } from 'viem/chains'
+  import { reconnect } from '@wagmi/core'
+
+  // 1. Define constants
+  const projectId = 'YOUR_PROJECT_ID'
+
+  // 2. Create wagmiConfig
+  const metadata = {
+    name: 'Web3Modal',
+    description: 'Web3Modal Example',
+    url: 'https://web3modal.com', // origin must match your domain & subdomain
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+  }
+
+  const chains = [mainnet, arbitrum]
+  const config = defaultWagmiConfig({
+    chains, // required
+    projectId, // required
+    metadata, // required
+    enableWalletConnect: true, // Optional - true by default
+    enableInjected: true, // Optional - true by default
+    enableEIP6963: true, // Optional - true by default
+    enableCoinbase: true // Optional - true by default
+  })
+
+  reconnect(config)
+  // 3. Create modal
+  createWeb3Modal({
+    wagmiConfig: config,
+    projectId,
+    enableAnalytics: true // Optional - defaults to your Cloud configuration
+  })
+</script>
+
+<template>
+  <w3m-button />
+</template>
+
+
+
