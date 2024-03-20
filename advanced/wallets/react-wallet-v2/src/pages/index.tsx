b309b0b0504eee821c0a11d7036d8415a94eb037
Luka Isailovic <lukaisailovic@gmail.com>
2024-03-20 08:14:13 +0000

    fix: smart account display (#515)
    * fix: safe smart account key
    
    * fix: display only from available smart accounts
    
    * fix: remove unused dependencies

--- advanced/wallets/react-wallet-v2/src/pages/index.tsx
+++ advanced/wallets/react-wallet-v2/src/pages/index.tsx
@@ -14,7 +14,7 @@ import SettingsStore from '@/store/SettingsStore'
 import { Text } from '@nextui-org/react'
 import { Fragment } from 'react'
 import { useSnapshot } from 'valtio'
-import { isAllowedKernelChain, isAllowedSafeChain } from '@/utils/SmartAccountUtil'
+import useSmartAccounts from '@/hooks/useSmartAccounts'
 
 export default function HomePage() {
   const {
@@ -28,10 +28,9 @@ export default function HomePage() {
     tronAddress,
     tezosAddress,
     kadenaAddress,
-    kernelSmartAccountAddress,
-    safeSmartAccountAddress,
     smartAccountEnabled
   } = useSnapshot(SettingsStore.state)
+  const { getAvailableSmartAccounts } = useSmartAccounts()
 
   return (
     <Fragment>
@@ -150,29 +149,23 @@ export default function HomePage() {
             if (smartAccountEnabled) {
               return (
                 <div key={`${name}-smart`} style={{ marginBottom: 10 }}>
-                  {isAllowedKernelChain(chainId) ? (
-                    <AccountCard
-                      key={`${name}-kernel`}
-                      name={`Kernel Smart Account \n ${name}`}
-                      logo={logo}
-                      rgb={rgb}
-                      address={kernelSmartAccountAddress}
-                      chainId={caip10.toString()}
-                      data-testid={`chain-card-${caip10.toString()}-kernel`}
-                    />
-                  ) : null}
-
-                  {isAllowedSafeChain(chainId) ? (
-                    <AccountCard
-                      key={`${name}-safe`}
-                      name={`Safe Smart Account \n ${name}`}
-                      logo={logo}
-                      rgb={rgb}
-                      address={safeSmartAccountAddress}
-                      chainId={caip10.toString()}
-                      data-testid={`chain-card-${caip10.toString()}-safe`}
-                    />
-                  ) : null}
+                  {getAvailableSmartAccounts()
+                    .filter(account => {
+                      return account.chain.id === chainId
+                    })
+                    .map(account => {
+                      return (
+                        <AccountCard
+                          key={`${name}-${account.type.toLowerCase()}`}
+                          name={`${account.type} Smart Account \n ${name}`}
+                          logo={logo}
+                          rgb={rgb}
+                          address={account.address}
+                          chainId={caip10.toString()}
+                          data-testid={`chain-card-${caip10.toString()}-${account.type.toLowerCase()}`}
+                        />
+                      )
+                    })}
                 </div>
               )
             }
