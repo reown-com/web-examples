a84ef23d9b2106ade8098107459220f0a9ea6b5b
Luka Isailovic <lukaisailovic@gmail.com>
2024-03-27 20:38:37 +0100

    chore: add Biconomy account implementation

--- advanced/wallets/react-wallet-v2/src/lib/smart-accounts/BiconomySmartAccountLib.ts
+++ advanced/wallets/react-wallet-v2/src/lib/smart-accounts/BiconomySmartAccountLib.ts
@@ -1,21 +1,21 @@
 import { ENTRYPOINT_ADDRESS_V06, SmartAccountClientConfig } from 'permissionless'
 import { SmartAccountLib } from './SmartAccountLib'
-import { SmartAccount, signerToSafeSmartAccount } from 'permissionless/accounts'
+import { SmartAccount, signerToBiconomySmartAccount, signerToSafeSmartAccount } from 'permissionless/accounts'
 import { EntryPoint } from 'permissionless/types/entrypoint'
 
-export class SafeSmartAccountLib extends SmartAccountLib {
+export class BiconomySmartAccountLib extends SmartAccountLib {
   async getClientConfig(): Promise<SmartAccountClientConfig<EntryPoint>> {
     if (this.entryPoint !== ENTRYPOINT_ADDRESS_V06) {
       throw new Error('Only entrypoint V6 is supported')
     }
 
-    const safeAccount = await signerToSafeSmartAccount(this.publicClient, {
+    const biconomyAccount = await signerToBiconomySmartAccount(this.publicClient, {
       entryPoint: this.entryPoint,
       signer: this.signer,
-      safeVersion: '1.4.1'
     })
+
     return {
-      account: safeAccount as SmartAccount<EntryPoint>,
+      account: biconomyAccount as SmartAccount<EntryPoint>,
       entryPoint: this.entryPoint,
       chain: this.chain,
       bundlerTransport: this.bundlerUrl,
