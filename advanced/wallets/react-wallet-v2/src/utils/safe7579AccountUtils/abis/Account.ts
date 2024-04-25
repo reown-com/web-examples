1100287832bb7a095abc3a8641bfb443b7424a4b
Karandeep Singh <karandeepsinghcaj@outlook.com>
2024-04-25 13:51:53 -0400

    sendTransaction with permissionValidator module

--- advanced/wallets/react-wallet-v2/src/utils/safe7579AccountUtils/abis/Account.ts
+++ advanced/wallets/react-wallet-v2/src/utils/safe7579AccountUtils/abis/Account.ts
@@ -16,3 +16,19 @@ export const executeAbi = [{
   "outputs": [],
   "stateMutability": "payable"
 }] as const
+
+export const isModuleInstalledAbi = [{
+  "type": "function",
+  "name": "isModuleInstalled",
+  "inputs": [
+    { "name": "moduleType", "type": "uint256", "internalType": "uint256" },
+    { "name": "module", "type": "address", "internalType": "address" },
+    {
+      "name": "additionalContext",
+      "type": "bytes",
+      "internalType": "bytes"
+    }
+  ],
+  "outputs": [{ "name": "", "type": "bool", "internalType": "bool" }],
+  "stateMutability": "view"
+}] as const
