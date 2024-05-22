10afd31ccf98dab054bc44c0df1b66b74917a6d5
Karandeep Singh <karandeepsinghcaj@outlook.com>
2024-05-22 14:52:02 -0400

    fix smart accounts data-testid

--- advanced/wallets/react-wallet-v2/src/pages/settings.tsx
+++ advanced/wallets/react-wallet-v2/src/pages/settings.tsx
@@ -83,7 +83,7 @@ export default function SettingsPage() {
                     <Switch
                       checked={kernelSmartAccountEnabled}
                       onChange={SettingsStore.toggleKernelSmartAccountsEnabled}
-                      data-testid="settings-toggle-smart-account-sponsorship"
+                      data-testid="settings-toggle-smart-account-zerodev"
                     />
                     <Text>{kernelSmartAccountEnabled ? 'Enabled' : 'Disabled'}</Text>
                   </Row>
@@ -95,7 +95,7 @@ export default function SettingsPage() {
                     <Switch
                       checked={safeSmartAccountEnabled}
                       onChange={SettingsStore.toggleSafeSmartAccountsEnabled}
-                      data-testid="settings-toggle-smart-account-sponsorship"
+                      data-testid="settings-toggle-smart-account-safe"
                     />
                     <Text>{safeSmartAccountEnabled ? 'Enabled' : 'Disabled'}</Text>
                   </Row>
@@ -107,7 +107,7 @@ export default function SettingsPage() {
                     <Switch
                       checked={biconomySmartAccountEnabled}
                       onChange={SettingsStore.toggleBiconomySmartAccountsEnabled}
-                      data-testid="settings-toggle-smart-account-sponsorship"
+                      data-testid="settings-toggle-smart-account-biconomy"
                     />
                     <Text>{biconomySmartAccountEnabled ? 'Enabled' : 'Disabled'}</Text>
                   </Row>
