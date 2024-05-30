93dc3118998d86d55876bd1aafae4f4012810007
Karandeep Singh <karandeepsinghcaj@outlook.com>
2024-05-30 12:18:23 -0400

    added ERC7715 data (method rpc and types)

---
+++ advanced/wallets/react-wallet-v2/src/data/EIP7715Data.ts
@@ -0,0 +1,50 @@
+/**
+ * EIP7715Method
+ */
+export const EIP7715_METHOD = {
+  WALLET_ISSUE_PERMISSIONS: 'wallet_issuePermissions',
+}
+
+export type IssuePermissionsRequestParams = {
+  signer?: {
+    type: {
+      name: string,
+      uuid?: string,
+    },
+    data: any
+  }
+ 
+  permissions: {
+    type: {
+	    name: string,
+      uuid?: string,
+    }
+    data: any
+    required: boolean
+  }[]
+  
+  expiry: number,
+}
+
+
+export type IssuePermissionsResponse = {
+  grantedPermissions: {
+    type: {
+      name: string,
+      uuid?: string,
+    },
+    data: any
+  }[]
+      
+  expiry: number
+  
+  signerData: {
+    userOpBuilder?: `0x${string}`
+    submitToAddress?: `0x${string}`
+  },
+  
+	factory?: `0x${string}`,
+	factoryData?: string,
+	
+  permissionsContext: string
+}
