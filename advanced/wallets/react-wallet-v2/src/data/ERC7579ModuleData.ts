0e4a06f18881db6dbe6d3687d8e1062db2167f8d
Karandeep Singh <karandeepsinghcaj@outlook.com>
2024-06-13 19:35:23 -0400

    chore: text change

--- advanced/wallets/react-wallet-v2/src/data/ERC7579ModuleData.ts
+++ advanced/wallets/react-wallet-v2/src/data/ERC7579ModuleData.ts
@@ -1,13 +1,20 @@
-//ES6 syntax dont work for this package
+//Note: ES6 syntax dont work for this package
 // import { MULTI_FACTOR_VALIDATOR_ADDRESS } from '@rhinestone/module-sdk'
-const moduleSdk = require('@rhinestone/module-sdk')
+const {
+  MULTI_FACTOR_VALIDATOR_ADDRESS,
+  OWNABLE_VALIDATOR_ADDRESS,
+  WEBAUTHN_VALIDATOR_ADDRESS,
+  SCHEDULED_ORDERS_EXECUTER_ADDRESS,
+  SCHEDULED_TRANSFERS_EXECUTER_ADDRESS,
+} = require('@rhinestone/module-sdk') as typeof import('@rhinestone/module-sdk')
+
 export const moduleTypeIds = {
   validator: 1,
   executor: 2,
   fallback: 3,
   hook: 4
 }
-const PERMISSION_VALIDATOR_ADDRESS = '0x6671AD9ED29E2d7a894E80bf48b7Bf03Ee64A0f4'
+export const PERMISSION_VALIDATOR_ADDRESS = '0x6671AD9ED29E2d7a894E80bf48b7Bf03Ee64A0f4'
 
 export type Module = {
   name: string
@@ -25,39 +32,39 @@ export const supportedModules: Module[] = [
     moduleData: '0x'
   },
   {
-    name: `Muti-factor Validation`,
+    name: 'Ownable Validator',
     type: 1,
-    moduleAddress: moduleSdk.MULTI_FACTOR_VALIDATOR_ADDRESS,
-    description: `The MFA Validator module is a module that allows you to add multi-factor validation to an account. The MFA Validator module is used to validate transactions and other executions on the account.`,
-    moduleData: ''
-  },
-  {
-    name: 'Ownable validator',
-    type: 1,
-    moduleAddress: moduleSdk.OWNABLE_VALIDATOR_ADDRESS,
+    moduleAddress: OWNABLE_VALIDATOR_ADDRESS,
     description: `The Ownable Validator module is a module that allows you to add multiple ECDSA owners to an account.
      The owners can then be used to sign transactions to be executed on the account.`,
     moduleData: ''
   },
   {
-    name: 'WebAuthn validator',
+    name: `Muti-factor Validator`,
     type: 1,
-    moduleAddress: moduleSdk.WEBAUTHN_VALIDATOR_ADDRESS,
+    moduleAddress: MULTI_FACTOR_VALIDATOR_ADDRESS,
+    description: `The MFA Validator module is a module that allows you to add multi-factor validation to an account. The MFA Validator module is used to validate transactions and other executions on the account.`,
+    moduleData: ''
+  },
+  {
+    name: 'WebAuthn Validator',
+    type: 1,
+    moduleAddress: WEBAUTHN_VALIDATOR_ADDRESS,
     description: 'Coming Soon',
     moduleData: ''
   },
   {
     name: 'Schedule Orders Executor',
     type: 2,
-    moduleAddress: moduleSdk.SCHEDULED_ORDERS_EXECUTER_ADDRESS,
-    description: 'Coming Soon',
+    moduleAddress: SCHEDULED_ORDERS_EXECUTER_ADDRESS,
+    description: `The Scheduled Orders module allows users to schedule swaps to be executed at a later time, with an optional recurring schedule. This module is an executor that is installed on an account and can be triggered by an automation service at the pre-specified time(s).`,
     moduleData: ''
   },
   {
     name: 'Schedule Transfers Executor',
     type: 2,
-    moduleAddress: moduleSdk.SCHEDULED_TRANSFERS_EXECUTER_ADDRESS,
-    description: 'Coming Soon',
+    moduleAddress: SCHEDULED_TRANSFERS_EXECUTER_ADDRESS,
+    description: `The Scheduled Transfers module allows users to schedule token transfers to occur at a future time, with an optional recurring schedule. It is an executor that is installed on an account and can be triggered by an automation service at the pre-specified time(s).`,
     moduleData: ''
   }
 ]
