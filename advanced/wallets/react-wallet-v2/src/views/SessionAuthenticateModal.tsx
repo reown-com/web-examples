9dccc6f17ed4255371186623cfb6cf95599b7aee
Karandeep Singh <karandeepsinghcaj@outlook.com>
2024-06-27 23:33:24 -0400

    Parsing recap for permissions

--- advanced/wallets/react-wallet-v2/src/views/SessionAuthenticateModal.tsx
+++ advanced/wallets/react-wallet-v2/src/views/SessionAuthenticateModal.tsx
@@ -2,33 +2,70 @@ import { Fragment, useCallback, useEffect, useMemo, useState } from 'react'
 import { useSnapshot } from 'valtio'
 import { Col, Divider, Row, Text, Code, Checkbox, Grid } from '@nextui-org/react'
 import { buildAuthObject, getSdkError, populateAuthPayload } from '@walletconnect/utils'
-
+import { decodeRecap } from '@walletconnect/utils'
 import ModalFooter from '@/components/ModalFooter'
 import ProjectInfoCard from '@/components/ProjectInfoCard'
 import RequestModalContainer from '@/components/RequestModalContainer'
 import VerifyInfobox from '@/components/VerifyInfobox'
 import ModalStore from '@/store/ModalStore'
 import SettingsStore from '@/store/SettingsStore'
-import { eip155Addresses, eip155Wallets } from '@/utils/EIP155WalletUtil'
+import { eip155Addresses, eip155Wallets, getWallet } from '@/utils/EIP155WalletUtil'
 import { web3wallet } from '@/utils/WalletConnectUtil'
 import RequestModal from './RequestModal'
 import { EIP155_CHAINS, EIP155_SIGNING_METHODS } from '@/data/EIP155Data'
 import { styledToast } from '@/utils/HelperUtil'
+import usePriorityAccounts from '@/hooks/usePriorityAccounts'
+import { smartAccountWallets } from '@/utils/SmartAccountUtil'
+import { SafeSmartAccountLib } from '@/lib/smart-accounts/SafeSmartAccountLib'
 
 export default function SessionAuthenticateModal() {
   // Get request and wallet data from store
   const authRequest = ModalStore.state.data?.authRequest
 
-  const { account } = useSnapshot(SettingsStore.state)
+  const {
+    account,
+    smartAccountEnabled,
+    safeSmartAccountEnabled,
+    safeSmartAccountAddress,
+    kernelSmartAccountAddress,
+    kernelSmartAccountEnabled,
+    biconomySmartAccountAddress,
+    biconomySmartAccountEnabled
+  } = useSnapshot(SettingsStore.state)
   const [messages, setMessages] = useState<
     { authPayload: any; message: string; id: number; iss: string }[]
   >([])
   const [supportedChains] = useState<string[]>(Object.keys(EIP155_CHAINS))
   const [supportedMethods] = useState<string[]>(Object.values(EIP155_SIGNING_METHODS))
   const [signStrategy, setSignStrategy] = useState(1)
-  // Ensure request and wallet are defined
 
-  const address = eip155Addresses[account]
+  const priorityAccounts = usePriorityAccounts({})
+  console.log({ authPayload: authRequest?.params.authPayload })
+  console.log({ priorityAccounts })
+  // Ensure request and wallet are defined
+  const address = smartAccountEnabled
+    ? safeSmartAccountEnabled
+      ? safeSmartAccountAddress
+      : kernelSmartAccountEnabled
+      ? kernelSmartAccountAddress
+      : biconomySmartAccountEnabled
+      ? biconomySmartAccountAddress
+      : eip155Addresses[account]
+    : eip155Addresses[account]
+
+  function extractAndParsePermissionsParameters(inputString: string) {
+    // Remove the prefix
+    const jsonString = inputString.replace('submit transactions with permissons/', '')
+
+    try {
+      // Parse the remaining string as JSON
+      const jsonObject = JSON.parse(jsonString)
+      return jsonObject
+    } catch (error) {
+      console.error('Error parsing JSON:', error)
+      return null
+    }
+  }
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const getMessageToSign = useCallback(
@@ -42,6 +79,25 @@ export default function SessionAuthenticateModal() {
     },
     [address]
   )
+
+  function getSmartWallet(accountAddress: string, chainId: string) {
+    const smartAccount = `${chainId}:${accountAddress}`
+    const account = Object.keys(smartAccountWallets).find(sca => {
+      return sca.toLowerCase() === smartAccount.toLowerCase()
+    })
+    if (account) {
+      const lib = smartAccountWallets[account]
+      if (lib) {
+        return lib
+      }
+    }
+  }
+
+  const extractURI = (message: string) => {
+    const uriRegex = /URI:\s*(https?:\/\/[^\s]+)/
+    const match = message.match(uriRegex)
+    return match ? match[1] : null
+  }
 
   useEffect(() => {
     if (!authRequest?.params?.authPayload) return
@@ -90,23 +146,62 @@ export default function SessionAuthenticateModal() {
     }
   }, [address, authRequest, getMessageToSign, signStrategy, supportedChains, supportedMethods])
 
-  // Handle approve action (logic varies based on request method)
   const onApprove = useCallback(async () => {
     if (messages.length) {
       const signedAuths = []
       for (const message of messages) {
-        const signature = await eip155Wallets[address].signMessage(message.message)
-        const signedCacao = buildAuthObject(
-          message.authPayload,
-          {
-            t: 'eip191',
-            s: signature
-          },
-          message.iss
-        )
-        signedAuths.push(signedCacao)
+        let signature = '0x'
+        if (smartAccountEnabled) {
+          let grantedPermissions = undefined
+          const chainId = message.iss.split(':')[1]
+          const accountAddress = message.iss.split(':')[2]
+          const scw = getSmartWallet(accountAddress, chainId)
+          if(!scw) return
+            signature = await scw.signMessage(message.message)
+            console.log({signature})
+          if (scw && scw instanceof SafeSmartAccountLib) {
+            const msg = message.message
+            const uri = extractURI(msg);
+            console.log("Extracted URI:", uri);
+            if(uri) {
+              const recapIndex = msg.lastIndexOf('recap:')
+              const recapString = msg.substring(recapIndex+6)
+              const recapType = decodeRecap(recapString)
+              const recap = recapType['att'][uri]
+              console.log(Object.keys(recap)[0])
+              const permissionsParameter = extractAndParsePermissionsParameters(Object.keys(recap)[0] as unknown as string)
+              console.log(permissionsParameter)
+              if(permissionsParameter){
+                grantedPermissions = await scw.grantPermissions(permissionsParameter)
+                console.log(grantedPermissions)
+              }
+            }
+            
+          }
+          const signedCacao = buildAuthObject(
+            message.authPayload,
+            {
+              t: 'eip1271',
+              s: signature
+            },
+            message.iss
+          )
+          grantedPermissions && signedCacao.p.resources?.push(JSON.stringify(grantedPermissions))
+          signedAuths.push(signedCacao)
+        } else {
+          signature = await eip155Wallets[address].signMessage(message.message)
+          const signedCacao = buildAuthObject(
+            message.authPayload,
+            {
+              t: 'eip191',
+              s: signature
+            },
+            message.iss
+          )
+          signedAuths.push(signedCacao)
+        }
       }
-
+      console.log({signedAuths})
       await web3wallet.engine.signClient.approveSessionAuthenticate({
         id: messages[0].id,
         auths: signedAuths
