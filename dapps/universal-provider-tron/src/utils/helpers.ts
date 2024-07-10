8dd9820abe69da28efac514f0c8a6d3c2deeebc1
Tomas Rawski <rtomas@gmail.com>
2024-07-10 13:38:05 -0300

    WIP: first commit of the tron example

---
+++ dapps/universal-provider-tron/src/utils/helpers.ts
@@ -0,0 +1,67 @@
+
+
+import UniversalProvider from "@walletconnect/universal-provider/dist/types/UniversalProvider";
+import bs58 from "bs58";
+import nacl from "tweetnacl";
+
+
+export enum TronChains {
+  Mainnet = "0x2b6653dc",
+  Devnet = "0xcd8690dc",
+}
+
+export const blockchainName = "tron";
+
+export const getProviderUrl = (chainId: string) => {
+  return `https://rpc.walletconnect.com/v1/?chainId=${chainId}&projectId=${
+    import.meta.env.VITE_PROJECT_ID
+  }`;
+};
+
+export const signMessage = async (
+  message: string,
+  provider: UniversalProvider,
+  address: string
+) => {
+
+  try {
+    
+    const result = await provider!.request<{ signature: string }>({
+      method: "tron_signMessage",
+      params: {
+        address,
+        message,
+      },
+    });
+
+    return {
+      method: "tron_signMessage",
+      address,
+      valid: true,
+      result: result.signature,
+    };
+
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
+
+export const sendTransaction = async (
+  to: string,
+  amount: number,
+  provider: UniversalProvider,
+  address: string
+) => {
+  const isTestnet = provider.session!.namespaces.tron.chains?.includes(
+    `${blockchainName}:${TronChains.Devnet}`
+  );
+
+ 
+    return {
+      method: "tron_signTransaction",
+      address,
+      valid: true, //valid,
+      result: "" //result.signature,
+    };
+
+};
