17c62d4512ac22b3cc0c86a2d458904ae0c18080
Felipe Mendes <zo.fmendes@gmail.com>
2024-08-08 10:57:46 -0300

    refactor: solana wallet rpc interface (#672)
    * refactor: update solana rpc interface for serialized transactions
    
    * feat: handle error response for failed solana requests

--- advanced/wallets/react-wallet-v2/src/lib/SolanaLib.ts
+++ advanced/wallets/react-wallet-v2/src/lib/SolanaLib.ts
@@ -1,14 +1,6 @@
-import {
-  Keypair,
-  Connection,
-  Transaction,
-  TransactionInstruction,
-  PublicKey,
-  SendOptions
-} from '@solana/web3.js'
+import { Keypair, Connection, SendOptions, VersionedTransaction } from '@solana/web3.js'
 import bs58 from 'bs58'
 import nacl from 'tweetnacl'
-import SolanaWallet, { SolanaSignTransaction } from 'solana-wallet'
 import { SOLANA_MAINNET_CHAINS, SOLANA_TEST_CHAINS } from '@/data/SolanaData'
 
 /**
@@ -23,11 +15,9 @@ interface IInitArguments {
  */
 export default class SolanaLib {
   keypair: Keypair
-  solanaWallet: SolanaWallet
 
   constructor(keypair: Keypair) {
     this.keypair = keypair
-    this.solanaWallet = new SolanaWallet(Buffer.from(keypair.secretKey))
   }
 
   static init({ secretKey }: IInitArguments) {
@@ -44,35 +34,28 @@ export default class SolanaLib {
     return this.keypair.secretKey.toString()
   }
 
-  public async signMessage(message: string) {
-    const signature = nacl.sign.detached(bs58.decode(message), this.keypair.secretKey)
+  public async signMessage(
+    params: SolanaLib.SignMessage['params']
+  ): Promise<SolanaLib.SignMessage['result']> {
+    const signature = nacl.sign.detached(bs58.decode(params.message), this.keypair.secretKey)
     const bs58Signature = bs58.encode(signature)
 
     return { signature: bs58Signature }
   }
 
   public async signTransaction(
-    feePayer: SolanaSignTransaction['feePayer'],
-    recentBlockhash: SolanaSignTransaction['recentBlockhash'],
-    instructions: SolanaSignTransaction['instructions'],
-    partialSignatures?: SolanaSignTransaction['partialSignatures']
-  ) {
-    const { signature } = await this.solanaWallet.signTransaction(feePayer, {
-      feePayer,
-      instructions,
-      recentBlockhash,
-      partialSignatures: partialSignatures ?? []
-    })
+    params: SolanaLib.SignTransaction['params']
+  ): Promise<SolanaLib.SignTransaction['result']> {
+    const transaction = this.deserialize(params.transaction)
+    this.sign(transaction)
 
-    return { signature }
+    return { transaction: this.serialize(transaction) }
   }
 
   public async signAndSendTransaction(
-    feePayer: SolanaSignTransaction['feePayer'],
-    instructions: SolanaSignTransaction['instructions'],
-    chainId: string,
-    options: SendOptions = {}
-  ) {
+    params: SolanaLib.SignAndSendTransaction['params'],
+    chainId: string
+  ): Promise<SolanaLib.SignAndSendTransaction['result']> {
     const rpc = { ...SOLANA_TEST_CHAINS, ...SOLANA_MAINNET_CHAINS }[chainId]?.rpc
 
     if (!rpc) {
@@ -80,38 +63,43 @@ export default class SolanaLib {
     }
 
     const connection = new Connection(rpc)
+    const transaction = this.deserialize(params.transaction)
+    this.sign(transaction)
 
-    const parsedInstructions = instructions.map(instruction => {
-      const keys = instruction.keys.map(key => ({
-        pubkey: new PublicKey(key.pubkey),
-        isSigner: key.isSigner,
-        isWritable: key.isWritable
-      }))
-      const programId = new PublicKey(instruction.programId)
-      const data =
-        typeof instruction.data === 'string'
-          ? Buffer.from(bs58.decode(instruction.data).buffer)
-          : instruction.data
-
-      return new TransactionInstruction({
-        keys,
-        programId,
-        data
-      })
+    const signature = await connection.sendTransaction(transaction, {
+      maxRetries: 3,
+      preflightCommitment: 'recent',
+      ...params.options
     })
-
-    const transaction = new Transaction().add(...parsedInstructions)
-    transaction.feePayer = new PublicKey(feePayer)
-    transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
-    transaction.sign(this.keypair)
-
-    const signature = await connection.sendRawTransaction(transaction.serialize())
-    const confirmation = await connection.confirmTransaction(signature, options.preflightCommitment)
-
-    if (confirmation.value.err) {
-      throw new Error(confirmation.value.err.toString())
-    }
 
     return { signature }
   }
+
+  private serialize(transaction: VersionedTransaction): string {
+    return bs58.encode(transaction.serialize())
+  }
+
+  private deserialize(transaction: string): VersionedTransaction {
+    return VersionedTransaction.deserialize(bs58.decode(transaction))
+  }
+
+  private sign(transaction: VersionedTransaction) {
+    transaction.sign([this.keypair])
+  }
+}
+
+export namespace SolanaLib {
+  type RPCRequest<Params, Result> = {
+    params: Params
+    result: Result
+  }
+
+  export type SignMessage = RPCRequest<{ message: string }, { signature: string }>
+
+  export type SignTransaction = RPCRequest<{ transaction: string }, { transaction: string }>
+
+  export type SignAndSendTransaction = RPCRequest<
+    { transaction: string; options?: SendOptions },
+    { signature: string }
+  >
 }
--- advanced/wallets/react-wallet-v2/src/utils/SolanaRequestHandlerUtil.ts
+++ advanced/wallets/react-wallet-v2/src/utils/SolanaRequestHandlerUtil.ts
@@ -12,32 +12,28 @@ export async function approveSolanaRequest(
   const { request, chainId } = params
   const wallet = solanaWallets[getWalletAddressFromParams(solanaAddresses, params)]
 
-  switch (request.method) {
-    case SOLANA_SIGNING_METHODS.SOLANA_SIGN_MESSAGE:
-      const signedMessage = await wallet.signMessage(request.params.message)
-      return formatJsonRpcResult(id, signedMessage)
+  try {
+    switch (request.method) {
+      case SOLANA_SIGNING_METHODS.SOLANA_SIGN_MESSAGE:
+        const signedMessage = await wallet.signMessage(request.params)
+        return formatJsonRpcResult(id, signedMessage)
 
-    case SOLANA_SIGNING_METHODS.SOLANA_SIGN_TRANSACTION:
-      const signedTransaction = await wallet.signTransaction(
-        request.params.feePayer,
-        request.params.recentBlockhash,
-        request.params.instructions
-      )
+      case SOLANA_SIGNING_METHODS.SOLANA_SIGN_TRANSACTION:
+        const signedTransaction = await wallet.signTransaction(request.params)
+        return formatJsonRpcResult(id, signedTransaction)
 
-      return formatJsonRpcResult(id, signedTransaction)
+      case SOLANA_SIGNING_METHODS.SOLANA_SIGN_AND_SEND_TRANSACTION:
+        const signedAndSentTransaction = await wallet.signAndSendTransaction(
+          request.params,
+          chainId
+        )
+        return formatJsonRpcResult(id, signedAndSentTransaction)
 
-    case SOLANA_SIGNING_METHODS.SOLANA_SIGN_AND_SEND_TRANSACTION:
-      const signedAndSentTransaction = await wallet.signAndSendTransaction(
-        request.params.feePayer,
-        request.params.instructions,
-        chainId,
-        request.params.options
-      )
-
-      return formatJsonRpcResult(id, signedAndSentTransaction)
-
-    default:
-      throw new Error(getSdkError('INVALID_METHOD').message)
+      default:
+        throw new Error(getSdkError('INVALID_METHOD').message)
+    }
+  } catch (error) {
+    return formatJsonRpcError(id, (error as Error)?.message)
   }
 }
 
