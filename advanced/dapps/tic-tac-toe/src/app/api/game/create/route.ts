e2907f48e56cdf6b30f4896c9e3a11dc9673e4a6
Karandeep Singh <karandeepsinghcaj@outlook.com>
2024-08-30 14:45:24 -0400

    handle app privatekey

--- advanced/dapps/tic-tac-toe/src/app/api/game/create/route.ts
+++ advanced/dapps/tic-tac-toe/src/app/api/game/create/route.ts
@@ -1,26 +1,42 @@
-import { NextResponse } from 'next/server';
-import { isAddress } from 'viem';
-import { GrantPermissionsReturnType } from 'viem/experimental';
-import { createGame } from '@/utils/TicTacToeUtils';
+import { createGame } from "@/utils/TicTacToeUtils";
+import { NextResponse } from "next/server";
+import { isAddress } from "viem";
+import { GrantPermissionsReturnType } from "viem/experimental";
 
 export async function POST(request: Request) {
   try {
+    const APPLICATION_PRIVATE_KEY = process.env.APPLICATION_PRIVATE_KEY;
+    if (!APPLICATION_PRIVATE_KEY) {
+      return NextResponse.json(
+        { message: "Missing required environment variables" },
+        { status: 400 },
+      );
+    }
     const { permissions, pci } = await request.json();
 
-    if (!permissions) throw new Error('No permissions provided');
-    if (!pci) throw new Error('No pci provided');
+    if (!permissions) {
+      return NextResponse.json(
+        { message: "No permissions provided" },
+        { status: 400 },
+      );
+    }
+    if (!pci) {
+      return NextResponse.json({ message: "No PCI provided" }, { status: 400 });
+    }
 
-    const playerOAddress = (permissions as GrantPermissionsReturnType).signerData?.submitToAddress;
-    if (!playerOAddress || !isAddress(playerOAddress)) throw new Error('Invalid playerO address');
+    const playerOAddress = (permissions as GrantPermissionsReturnType)
+      .signerData?.submitToAddress;
+    if (!playerOAddress || !isAddress(playerOAddress))
+      throw new Error("Invalid playerO address");
 
-    const txHash = await createGame(playerOAddress);
+    const txHash = await createGame(APPLICATION_PRIVATE_KEY, playerOAddress);
 
-    return NextResponse.json({transactionHash : txHash});
+    return NextResponse.json({ transactionHash: txHash });
   } catch (e) {
-    console.error('Error:', e);
+    console.error("Error:", e);
     return NextResponse.json(
-      { message: 'An error occurred', error: (e as Error).message },
-      { status: 500 }
+      { message: "An error occurred", error: (e as Error).message },
+      { status: 500 },
     );
   }
 }
