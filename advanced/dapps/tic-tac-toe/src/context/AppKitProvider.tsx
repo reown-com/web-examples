abfd4a383f0b4e44fc787733be54750b9a025f80
Karandeep Singh <karandeepsinghcaj@outlook.com>
2024-08-30 04:41:43 -0400

    updated configs

--- advanced/dapps/tic-tac-toe/src/context/AppKitProvider.tsx
+++ advanced/dapps/tic-tac-toe/src/context/AppKitProvider.tsx
@@ -1,22 +1,21 @@
-'use client'
-import React, { ReactNode } from 'react'
-import { createWeb3Modal } from '@web3modal/wagmi/react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { State, WagmiProvider } from 'wagmi'
-import { walletConnect } from '@wagmi/connectors'
-import { OPTIONAL_METHODS } from '@walletconnect/ethereum-provider'
-import { Theme } from '@radix-ui/themes'
-import { getWagmiConfig } from '../utils/WagmiConstants'
-import { ConstantsUtil } from '../utils/ConstantsUtil'
+"use client";
+import React, { ReactNode } from "react";
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { State, WagmiProvider } from "wagmi";
+import { walletConnect } from "@wagmi/connectors";
+import { OPTIONAL_METHODS } from "@walletconnect/ethereum-provider";
+import { getWagmiConfig } from "../utils/WagmiConstants";
+import { ConstantsUtil } from "../utils/ConstantsUtil";
 
 // Get projectId from https://cloud.walletconnect.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
 if (!projectId) {
-  throw new Error('Project ID is not defined')
+  throw new Error("Project ID is not defined");
 }
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const connectors = [
   walletConnect({
@@ -24,34 +23,31 @@ const connectors = [
     metadata: ConstantsUtil.Metadata,
     showQrModal: false,
     // @ts-expect-error: Overridding optionalMethods
-    optionalMethods: [...OPTIONAL_METHODS, 'wallet_grantPermissions']
-  })
-]
-export const wagmiEmailConfig = getWagmiConfig('email', connectors)
+    optionalMethods: [...OPTIONAL_METHODS, "wallet_grantPermissions"],
+  }),
+];
+export const wagmiEmailConfig = getWagmiConfig("email", connectors);
 
 createWeb3Modal({
   wagmiConfig: wagmiEmailConfig,
   projectId: ConstantsUtil.ProjectId,
   enableAnalytics: true,
   metadata: ConstantsUtil.Metadata,
-  termsConditionsUrl: 'https://walletconnect.com/terms',
-  privacyPolicyUrl: 'https://walletconnect.com/privacy'
-})
+  termsConditionsUrl: "https://walletconnect.com/terms",
+  privacyPolicyUrl: "https://walletconnect.com/privacy",
+  themeMode: "light",
+});
 
 export function AppKitProvider({
   children,
-  initialState
+  initialState,
 }: {
-  children: ReactNode
-  initialState?: State
+  children: ReactNode;
+  initialState?: State;
 }) {
   return (
-    <Theme appearance="dark">
-      <WagmiProvider config={wagmiEmailConfig} initialState={initialState}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </WagmiProvider>
-    </Theme>
-  )
+    <WagmiProvider config={wagmiEmailConfig} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
 }
