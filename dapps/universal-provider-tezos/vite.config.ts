421d44c8466a20218480bb285fa94a3c7d9fec78
dianasavvatina <diana.savvatina@trili.tech>
2024-09-05 11:17:59 +0100

    feat: added universal-provider-tezos dapp example

---
+++ dapps/universal-provider-tezos/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
+});
