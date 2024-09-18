5d448b79e44e4539fe01c890e60b56f926f37d13
Karandeep Singh <karandeepsinghcaj@outlook.com>
2024-09-18 00:07:53 -0400

    fix:remove duplicate chain declaration

--- advanced/wallets/react-wallet-v2/src/data/EIP155Data.ts
+++ advanced/wallets/react-wallet-v2/src/data/EIP155Data.ts
@@ -99,15 +99,6 @@ export const EIP155_TEST_CHAINS: Record<string, EIP155Chain> = {
     namespace: 'eip155',
     smartAccountEnabled: true
   },
-  'eip155:84532': {
-    chainId: 84532,
-    name: 'Base Sepolia',
-    logo: '/chain-logos/eip155-1.png',
-    rgb: '99, 125, 234',
-    rpc: 'https://sepolia.base.org',
-    namespace: 'eip155',
-    smartAccountEnabled: true
-  },
   'eip155:43113': {
     chainId: 43113,
     name: 'Avalanche Fuji',
@@ -155,7 +146,8 @@ export const EIP155_TEST_CHAINS: Record<string, EIP155Chain> = {
     logo: '/chain-logos/base.webp',
     rgb: '22, 83, 241',
     rpc: 'https://sepolia.base.org',
-    namespace: 'eip155'
+    namespace: 'eip155',
+    smartAccountEnabled: true
   }
 }
 
