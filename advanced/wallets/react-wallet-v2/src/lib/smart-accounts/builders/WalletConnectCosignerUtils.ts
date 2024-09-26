a40c79aa8bc093ef58af158a94b22a423da0c65e
Karandeep Singh <karandeepsinghcaj@outlook.com>
2024-09-26 19:41:57 -0400

    update WalletConnectCosigner as per new spec

--- advanced/wallets/react-wallet-v2/src/lib/smart-accounts/builders/WalletConnectCosignerUtils.ts
+++ advanced/wallets/react-wallet-v2/src/lib/smart-accounts/builders/WalletConnectCosignerUtils.ts
@@ -1,50 +1,86 @@
-import axios, { AxiosError } from 'axios'
+import axios, { Method, AxiosError } from 'axios'
 import { UserOperationWithBigIntAsHex } from './UserOpBuilder'
 import { bigIntReplacer } from '@/utils/HelperUtil'
 import { WC_COSIGNER_BASE_URL } from '@/utils/ConstantsUtil'
 
-// Define types for the request and response
-type AddPermission = {
-  permissionType: string
-  data: string
-  required: boolean
-  onChainValidated: boolean
+/*
+ * A wallet is the signer for these permissions
+ * `data` is not necessary for this signer type as the wallet is both the signer and grantor of these permissions
+ */
+export type WalletSigner = {
+  type: 'wallet'
+  data: Record<string, unknown>
 }
 
-type AddPermissionRequest = {
-  permission: AddPermission
-}
+// The types of keys that are supported for the following `key` and `keys` signer types.
+export type KeyType = 'secp256r1' | 'secp256k1' | 'ed25519' | 'schnorr'
 
-export type AddPermissionResponse = {
-  pci: string
-  key: string
-}
-
-type Signer = {
-  type: string
+/*
+ * A signer representing a single key.
+ * "Key" types are explicitly secp256r1 (p256) or secp256k1, and the public keys are hex-encoded.
+ */
+export type KeySigner = {
+  type: 'key'
   data: {
-    ids: string[]
+    type: KeyType
+    publicKey: `0x${string}`
   }
 }
 
-type SignerData = {
-  userOpBuilder: string
+/*
+ * A signer representing a multisig signer.
+ * Each element of `publicKeys` are all explicitly the same `KeyType`, and the public keys are hex-encoded.
+ */
+export type MultiKeySigner = {
+  type: 'keys'
+  data: {
+    keys: {
+      type: KeyType
+      publicKey: `0x${string}`
+    }[]
+  }
 }
 
-type PermissionsContext = {
-  signer: Signer
+// An account that can be granted with permissions as in ERC-7710.
+export type AccountSigner = {
+  type: 'account'
+  data: {
+    address: `0x${string}`
+  }
+}
+
+export type Signer = WalletSigner | KeySigner | MultiKeySigner | AccountSigner
+
+export type SmartSessionGrantPermissionsRequest = {
+  chainId: `0x${string}`
+  address?: `0x${string}`
   expiry: number
-  signerData: SignerData
-  factory?: string
-  factoryData?: string
-  permissionsContext: string
+  signer: Signer
+  permissions: {
+    type: string
+    data: Record<string, unknown>
+  }[]
+  policies: {
+    type: string
+    data: Record<string, unknown>
+  }[]
 }
 
-type UpdatePermissionsContextRequest = {
+//--Cosigner Types----------------------------------------------------------------------- //
+export type AddPermissionRequest = SmartSessionGrantPermissionsRequest
+
+export type AddPermissionResponse = {
   pci: string
-  signature?: string
-  context: PermissionsContext
+  key: {
+    type: KeyType
+    publicKey: `0x${string}`
+  }
 }
+
+export type ActivatePermissionsRequest = {
+  pci: string
+  context: `0x${string}`
+} & AddPermissionRequest
 
 type RevokePermissionRequest = {
   pci: string
@@ -60,7 +96,19 @@ type CoSignResponse = {
   signature: `0x${string}`
 }
 
-// Define a custom error type
+type GetPermissionsContextRequest = {
+  pci: string
+}
+
+type GetPermissionsContextResponse = {
+  context: `0x${string}`
+}
+
+type GetPermissionsResponse = {
+  pci: string[]
+}
+
+// -- Custom Error Class --------------------------------------------------- //
 export class CoSignerApiError extends Error {
   constructor(public status: number, message: string) {
     super(message)
@@ -68,27 +116,41 @@ export class CoSignerApiError extends Error {
   }
 }
 
-// Function to send requests to the CoSigner API
-async function sendCoSignerRequest<
+// -- Helper Function for API Requests ------------------------------------- //
+export async function sendCoSignerRequest<
   TRequest,
   TResponse,
-  TQueryParams extends Record<string, string> = Record<string, never>
->(args: {
+  TQueryParams extends Record<string, string>
+>({
+  url,
+  method,
+  request,
+  queryParams = {} as TQueryParams,
+  headers,
+  transformRequest
+}: {
   url: string
-  data: TRequest
+  method: Method
+  request?: TRequest
   queryParams?: TQueryParams
   headers: Record<string, string>
   transformRequest?: (data: TRequest) => unknown
 }): Promise<TResponse> {
-  const { url, data, queryParams = {}, headers, transformRequest } = args
-  const transformedData = transformRequest ? transformRequest(data) : data
-
   try {
-    const response = await axios.post<TResponse>(url, transformedData, {
+    const config = {
+      method,
+      url,
       params: queryParams,
-      headers
-    })
+      headers,
+      data:
+        method !== 'GET'
+          ? transformRequest
+            ? transformRequest(request as TRequest)
+            : request
+          : undefined
+    }
 
+    const response = await axios(config)
     return response.data
   } catch (error) {
     if (axios.isAxiosError(error)) {
@@ -102,7 +164,6 @@ async function sendCoSignerRequest<
         throw new CoSignerApiError(500, 'Network error')
       }
     }
-    // Re-throw if it's not an Axios error
     throw error
   }
 }
@@ -117,7 +178,7 @@ export class WalletConnectCosigner {
     this.projectId = projectId
   }
 
-  async addPermission(address: string, permission: AddPermission): Promise<AddPermissionResponse> {
+  async addPermission(address: string, data: AddPermissionRequest): Promise<AddPermissionResponse> {
     const url = `${this.baseUrl}/${encodeURIComponent(address)}`
 
     return await sendCoSignerRequest<
@@ -126,20 +187,60 @@ export class WalletConnectCosigner {
       { projectId: string }
     >({
       url,
-      data: { permission },
+      method: 'POST',
+      request: data,
       queryParams: { projectId: this.projectId },
       headers: { 'Content-Type': 'application/json' }
     })
   }
 
-  async updatePermissionsContext(
+  async activatePermissions(
     address: string,
-    updateData: UpdatePermissionsContextRequest
+    updateData: ActivatePermissionsRequest
   ): Promise<void> {
-    const url = `${this.baseUrl}/${encodeURIComponent(address)}/context`
-    await sendCoSignerRequest<UpdatePermissionsContextRequest, never, { projectId: string }>({
+    const url = `${this.baseUrl}/${encodeURIComponent(address)}/activate`
+    await sendCoSignerRequest<ActivatePermissionsRequest, never, { projectId: string }>({
       url,
-      data: updateData,
+      method: 'POST',
+      request: updateData,
+      queryParams: { projectId: this.projectId },
+      headers: { 'Content-Type': 'application/json' }
+    })
+  }
+
+  async coSignUserOperation(address: string, coSignData: CoSignRequest): Promise<CoSignResponse> {
+    const url = `${this.baseUrl}/${encodeURIComponent(address)}/sign`
+
+    return await sendCoSignerRequest<CoSignRequest, CoSignResponse, { projectId: string }>({
+      url,
+      method: 'POST',
+      request: coSignData,
+      queryParams: { projectId: this.projectId },
+      headers: { 'Content-Type': 'application/json' },
+      transformRequest: (value: CoSignRequest) => JSON.stringify(value, bigIntReplacer)
+    })
+  }
+
+  async getPermissionsContext(
+    address: string,
+    getPermissionsContextRequest: GetPermissionsContextRequest
+  ): Promise<GetPermissionsContextResponse> {
+    // need to change the method to use POST method and pass pci in the body with url as /{address}/getcontext
+    const url = `${this.baseUrl}/${encodeURIComponent(address)}/${getPermissionsContextRequest.pci}`
+    return await sendCoSignerRequest<never, GetPermissionsContextResponse, { projectId: string }>({
+      url,
+      method: 'GET',
+      // request: getPermissionsContextRequest,
+      queryParams: { projectId: this.projectId },
+      headers: { 'Content-Type': 'application/json' }
+    })
+  }
+
+  async getPermissions(address: string): Promise<GetPermissionsResponse> {
+    const url = `${this.baseUrl}/${encodeURIComponent(address)}`
+    return await sendCoSignerRequest<never, GetPermissionsResponse, { projectId: string }>({
+      url,
+      method: 'GET',
       queryParams: { projectId: this.projectId },
       headers: { 'Content-Type': 'application/json' }
     })
@@ -149,21 +250,10 @@ export class WalletConnectCosigner {
     const url = `${this.baseUrl}/${encodeURIComponent(address)}/revoke`
     await sendCoSignerRequest<RevokePermissionRequest, never, { projectId: string }>({
       url,
-      data: revokeData,
+      method: 'POST',
+      request: revokeData,
       queryParams: { projectId: this.projectId },
       headers: { 'Content-Type': 'application/json' }
-    })
-  }
-
-  async coSignUserOperation(address: string, coSignData: CoSignRequest): Promise<CoSignResponse> {
-    const url = `${this.baseUrl}/${encodeURIComponent(address)}/sign?version=1`
-
-    return await sendCoSignerRequest<CoSignRequest, CoSignResponse, { projectId: string }>({
-      url,
-      data: coSignData,
-      queryParams: { projectId: this.projectId },
-      headers: { 'Content-Type': 'application/json' },
-      transformRequest: (value: CoSignRequest) => JSON.stringify(value, bigIntReplacer)
     })
   }
 }
