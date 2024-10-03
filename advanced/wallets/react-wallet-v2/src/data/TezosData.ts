18dc528a929b9787c84b936a43ed0a7641a33c46
dianasavvatina <diana.savvatina@trili.tech>
2024-10-03 14:37:55 +0100

    fix: react-wallet-v2: ghostnet

--- advanced/wallets/react-wallet-v2/src/data/TezosData.ts
+++ advanced/wallets/react-wallet-v2/src/data/TezosData.ts
@@ -28,9 +28,9 @@ export const TEZOS_MAINNET_CHAINS: Record<string, ChainMetadata> = {
 }
 
 export const TEZOS_TEST_CHAINS: Record<string, ChainMetadata> = {
-  'tezos:testnet': {
-    chainId: 'testnet',
-    name: 'Tezos Testnet',
+  'tezos:ghostnet': {
+    chainId: 'ghostnet',
+    name: 'Tezos Ghostnet',
     logo: '/chain-logos/tezos.svg',
     rgb: '44, 125, 247',
     rpc: 'https://rpc.ghostnet.teztnets.com',
