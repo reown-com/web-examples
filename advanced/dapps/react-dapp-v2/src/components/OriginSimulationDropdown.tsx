8bbab924b2539e85defefc26a2f02746b82fdda6
Gancho Radkov <ganchoradkov93@gmail.com>
2024-11-01 15:15:44 +0200

    fix: verify simulations

--- advanced/dapps/react-dapp-v2/src/components/OriginSimulationDropdown.tsx
+++ advanced/dapps/react-dapp-v2/src/components/OriginSimulationDropdown.tsx
@@ -27,7 +27,18 @@ const SelectOption = styled.option`
 `;
 
 const OriginSimulationDropdown = (props: OriginSimulationProps) => {
-  const { origin, show } = props;
+  const { show } = props;
+  const [selectedOrigin, setSelectedOrigin] = React.useState(props.origin);
+  React.useEffect(() => {
+    if (!show) {
+      return;
+    }
+
+    setSelectedOrigin(
+      localStorage.getItem("wallet_connect_dapp_origin") || props.origin
+    );
+  }, [show]);
+
   const setOrigin = React.useCallback((origin: string) => {
     localStorage.setItem("wallet_connect_dapp_origin", origin);
     location.reload();
@@ -36,13 +47,15 @@ const OriginSimulationDropdown = (props: OriginSimulationProps) => {
     <div>
       {show && (
         <SelectContainer
-          value={origin}
+          value={selectedOrigin}
           onChange={(e) => setOrigin(e?.target?.value)}
         >
           <option disabled>Origin Url:</option>
           {ORIGIN_OPTIONS.map((e, i) => {
+            const seleted = e.value === selectedOrigin;
+            console.log("selected", seleted, e.value);
             return (
-              <SelectOption key={i} value={e.value}>
+              <SelectOption key={i} value={e.value} selected={seleted}>
                 {e.label}
               </SelectOption>
             );
