4b6f2276da7a2f51f59555ee7bf20d9c80473c16
Gancho Radkov <43912948+ganchoradkov@users.noreply.github.com>
2024-11-05 12:34:24 +0200

    fix: `sendPsbt` to use correct network and dust limit increase (#763)
    * fix: `sendPsbt` correct network and dust limit increase
    
    * refactor: sets `checkSegwitAlways` to true

--- advanced/dapps/react-dapp-v2/src/chains/bip122.ts
+++ advanced/dapps/react-dapp-v2/src/chains/bip122.ts
@@ -2,6 +2,7 @@ import { NamespaceMetadata, ChainMetadata, ChainsMap } from "../helpers";
 
 export const BIP122_MAINNET = "000000000019d6689c085ae165831e93";
 export const BIP122_TESTNET = "000000000933ea01ad0ee984209779ba";
+export const BIP122_DUST_LIMIT = "1001";
 
 export const BtcChainData: ChainsMap = {
   [BIP122_MAINNET]: {
