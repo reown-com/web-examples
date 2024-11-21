bba669a60d9fb5e51883228c53441f9c93394866
Tomas R <rtomas@gmail.com>
2024-11-20 22:10:43 -0300

    improve design + funcionality react ethers example

--- dapps/appkit/react-ethers/src/App.tsx
+++ dapps/appkit/react-ethers/src/App.tsx
@@ -1,33 +1,13 @@
 import { createAppKit } from '@reown/appkit/react'
-import { EthersAdapter } from '@reown/appkit-adapter-ethers'
-
-// for custom networks visit -> https://docs.reown.com/appkit/react/core/custom-networks
-import { arbitrum, mainnet, polygon, acala, chiliz, berachainTestnetbArtio, AppKitNetwork, sepolia } from '@reown/appkit/networks'
-import { Hooks } from './components/hooks'
+import { networks, projectId, metadata, ethersAdapter } from './config'
+import { ActionButtonList } from './components/ActionButtonList'
+import { InfoList } from './components/InfoList'
 
 import "./App.css"
 
-// 1. Get projectId from https://cloud.reown.com
-const projectId = import.meta.env.VITE_PROJECT_ID
-if (!projectId) {
-  throw new Error('VITE_PROJECT_ID is not set')
-}
-
-// 2. Create a metadata object - optional
-const metadata = {
-  name: 'AppKit',
-  description: 'AppKit Example',
-  url: 'https://reown.com', // origin must match your domain & subdomain
-  icons: ['https://avatars.githubusercontent.com/u/179229932']
-}
-
-// 3. Set the networks
-const networks: [AppKitNetwork, ...AppKitNetwork[]] = [arbitrum, mainnet];
-
-
-// 4. Create a AppKit instance
-const modal = createAppKit({
-  adapters: [new EthersAdapter()],
+// Create a AppKit instance
+createAppKit({
+  adapters: [ethersAdapter],
   networks,
   metadata,
   projectId,
@@ -39,12 +19,12 @@ const modal = createAppKit({
 export function App() {
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <w3m-button />
-          <Hooks />
-          <p>
-            <button onClick={() => modal.adapters![0].connectionControllerClient?.disconnect()}>Disconnect JS</button>
-          </p>
+    <div className={"pages"}>
+      <img src="/reown.svg" alt="Reown" style={{ width: '150px', height: '150px' }} />
+      <h1>AppKit ethers React dApp Example</h1>
+          <appkit-button />
+          <ActionButtonList />
+          <InfoList />
     </div>
   )
 }
