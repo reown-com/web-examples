ac76661fd9df1b98fd6013d715cfb7f7e5eedbdf
Tomas R <rtomas@gmail.com>
2024-11-20 22:10:33 -0300

    improve design + funcionality react ethers5 example

---
+++ dapps/appkit/react-ethers5/src/components/ActionButtonList.tsx
@@ -0,0 +1,15 @@
+import { useDisconnect, useAppKit, useAppKitNetwork  } from '@reown/appkit/react'
+import { networks } from '../config'
+
+export const ActionButtonList = () => {
+    const { disconnect } = useDisconnect();
+    const { open } = useAppKit();
+    const { switchNetwork } = useAppKitNetwork();
+  return (
+    <div >
+        <button onClick={() => open()}>Open</button>
+        <button onClick={() => disconnect()}>Disconnect</button>
+        <button onClick={() => switchNetwork(networks[1]) }>Switch</button>
+    </div>
+  )
+}
