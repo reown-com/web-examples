75914915f257d4f84e7d4fd43395579ec61ce2d6
Karandeep Singh <karandeepsinghcaj@outlook.com>
2025-01-20 23:29:40 +0530

    chore: fix toast text color

--- advanced/dapps/chat-demo-agent/src/components/ui/alert.tsx
+++ advanced/dapps/chat-demo-agent/src/components/ui/alert.tsx
@@ -4,11 +4,11 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-secondary",
   {
     variants: {
       variant: {
-        default: "bg-background text-foreground",
+        default: "bg-background text-secondary",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
       },
