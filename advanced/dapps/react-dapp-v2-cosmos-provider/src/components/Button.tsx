151d91782b471394b7eb980ddc20a8737deaaf75
Ben Kremer <ben@reown.com>
2025-03-17 17:08:35 +0100

    chore(dapps): replace yarn w/ pnpm, audit + update all deps (#848)

--- advanced/dapps/react-dapp-v2-cosmos-provider/src/components/Button.tsx
+++ advanced/dapps/react-dapp-v2-cosmos-provider/src/components/Button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import styled from "styled-components";
+import { styled, keyframes } from "styled-components";
 import Loader from "./Loader";
 import { colors, fonts, shadows, transitions } from "../styles";
 
