2706e0f95a61327bfbad595e169ad45df0b31187
tomiir <tomas@walletconnect.com>
2025-10-06 16:16:00 +0200

    chore: use getAddress

--- advanced/wallets/react-wallet-v2/src/lib/TonLib.ts
+++ advanced/wallets/react-wallet-v2/src/lib/TonLib.ts
@@ -101,7 +101,7 @@ export default class TonLib {
 
     const dataToSign = this.getToSign(params)
     const signature = sign(dataToSign, this.keypair.secretKey as unknown as Buffer)
-    const addressStr = this.wallet.address.toString({ bounceable: false })
+    const addressStr = this.getAddress()
 
     const result = {
       signature: signature.toString('base64'),
